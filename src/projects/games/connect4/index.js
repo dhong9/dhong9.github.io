30ca2c97c5dffceac5ddc7f95c05d9e2a0a491d7
Daniel Hong <d_hong7@u.pacific.edu>
2023-01-29 15:19:16 -0800

    Ported p5 into Connect 4 page

--- src/projects/games/connect4/index.js
+++ src/projects/games/connect4/index.js
@@ -1,9 +1,25 @@
+import Sketch from "react-p5";
+
 function Connect4() {
-  return (
-    <div>
-      <h1>Connect 4</h1>
-    </div>
-  );
+  let x = 50;
+  const y = 50;
+
+  const setup = (p5, canvasParentRef) => {
+    // use parent to render the canvas in this ref
+    // (without that p5 will render the canvas outside of your component)
+    p5.createCanvas(500, 500).parent(canvasParentRef);
+  };
+
+  const draw = (p5) => {
+    p5.background(0);
+    p5.ellipse(x, y, 70, 70);
+    // NOTE: Do not use setState in the draw function or in functions that are executed
+    // in the draw function...
+    // please use normal variables or class properties for these purposes
+    x += 1;
+  };
+
+  return <Sketch setup={setup} draw={draw} />;
 }
 
 export default Connect4;
