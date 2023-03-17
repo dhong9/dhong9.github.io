4d1d493dee4ded5b328588a5e86ebfcb32f731b2
Daniel Hong <d_hong7@u.pacific.edu>
2023-03-17 13:06:36 -0700

    Added images of platforms

--- src/pages/Presentation/sections/data/pagesData.js
+++ src/pages/Presentation/sections/data/pagesData.js
@@ -13,29 +13,22 @@ Coded by www.creative-tim.com
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 
-const imagesPrefix =
-  "https://raw.githubusercontent.com/creativetimofficial/public-assets/master/material-design-system/presentation/pages";
+const imagesPrefix = "https://cdnb.artstation.com/p/assets/images/images/060/686";
 
 export default [
   {
-    image: `${imagesPrefix}/about-us.jpg`,
-    name: "About Us Page",
+    image: `${imagesPrefix}/881/large/daniel-hong-artstation.jpg`,
+    name: "ArtStation",
     route: "/pages/landing-pages/about-us",
   },
   {
-    image: `${imagesPrefix}/contact.jpg`,
-    name: "Contact Us Page",
+    image: `${imagesPrefix}/915/large/daniel-hong-wirestock.jpg`,
+    name: "Wirestock",
     route: "/pages/landing-pages/contact-us",
   },
   {
-    image:
-      "https://raw.githubusercontent.com/creativetimofficial/public-assets/master/material-design-system/presentation/account/signin-basic.jpg",
-    name: "Sign In Page",
-    route: "/pages/authentication/sign-in",
-  },
-  {
-    image: `${imagesPrefix}/author.jpg`,
-    name: "Author Page",
+    image: `${imagesPrefix}/941/large/daniel-hong-github.jpg`,
+    name: "Github",
     route: "/pages/landing-pages/author",
   },
 ];
