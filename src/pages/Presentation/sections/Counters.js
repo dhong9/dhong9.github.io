b962bbfd026ca675e4997ba39c0ca1d50337533a
Daniel Hong <d_hong7@u.pacific.edu>
2023-03-17 14:17:16 -0700

    Updated counters with own stats

--- src/pages/Presentation/sections/Counters.js
+++ src/pages/Presentation/sections/Counters.js
@@ -31,27 +31,27 @@ function Counters() {
         <Grid container item xs={12} lg={9} sx={{ mx: "auto" }}>
           <Grid item xs={12} md={4}>
             <DefaultCounterCard
-              count={70}
+              count={6}
               suffix="+"
-              title="Coded Elements"
-              description="From buttons, to inputs, navbars, alerts or cards, you are covered"
+              title="Coding Projects"
+              description="Games, interpreters, and more"
             />
           </Grid>
           <Grid item xs={12} md={4} display="flex">
             <Divider orientation="vertical" sx={{ display: { xs: "none", md: "block" }, mx: 0 }} />
             <DefaultCounterCard
-              count={15}
+              count={53}
               suffix="+"
-              title="Design Blocks"
-              description="Mix the sections, change the colors and unleash your creativity"
+              title="Art Projects"
+              description="Animations, prints, and static images"
             />
             <Divider orientation="vertical" sx={{ display: { xs: "none", md: "block" }, ml: 0 }} />
           </Grid>
           <Grid item xs={12} md={4}>
             <DefaultCounterCard
-              count={4}
-              title="Pages"
-              description="Save 3-4 weeks of work when you use our pre-made pages for your website"
+              count={3}
+              title="Portfolio Pages"
+              description="Repositories of coding and art projects"
             />
           </Grid>
         </Grid>
