67ad769dc675c377379f13e7f30b368c5d105146
Daniel Hong <d_hong7@u.pacific.edu>
2023-04-16 20:13:19 -0700

    Makes About the index page

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ Coded by www.creative-tim.com
 import { useEffect } from "react";
 
 // react-router components
-import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 // @mui material components
 import { ThemeProvider } from "@mui/material/styles";
@@ -24,7 +24,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 
 // Material Kit 2 React themes
 import theme from "assets/theme";
-import Presentation from "layouts/pages/presentation";
+import AboutUs from "pages/LandingPages/AboutUs";
 
 // Material Kit 2 React routes
 import routes from "routes";
@@ -56,8 +56,7 @@ export default function App() {
       <CssBaseline />
       <Routes>
         {getRoutes(routes)}
-        <Route path="/presentation" element={<Presentation />} />
-        <Route path="*" element={<Navigate to="/presentation" />} />
+        <Route index element={<AboutUs />} />
       </Routes>
     </ThemeProvider>
   );
