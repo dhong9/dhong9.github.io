ca29f96707ba5e87199ced21592824ed04069056
Daniel Hong <d_hong7@u.pacific.edu>
2023-04-16 20:15:22 -0700

    Index leads to Presentation page

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 
 // Material Kit 2 React themes
 import theme from "assets/theme";
-import AboutUs from "pages/LandingPages/AboutUs";
+import Presentation from "layouts/pages/presentation";
 
 // Material Kit 2 React routes
 import routes from "routes";
@@ -56,7 +56,7 @@ export default function App() {
       <CssBaseline />
       <Routes>
         {getRoutes(routes)}
-        <Route index element={<AboutUs />} />
+        <Route index element={<Presentation />} />
       </Routes>
     </ThemeProvider>
   );
