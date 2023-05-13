e41ae7af14c861221181fb8a42e331bd82298754
dhong9 <danielhong35@gmail.com>
2023-05-13 13:45:17 -0700

    Added transition to snackbar

--- src/components/DHSnackbar/index.js
+++ src/components/DHSnackbar/index.js
@@ -2,9 +2,12 @@ import { forwardRef } from "react";
 
 import Snackbar from "@mui/material/Snackbar";
 import MuiAlert from "@mui/material/Alert";
+import Slide from "@mui/material/Slide";
 
 // prop-types is a library for typechecking of props
 import PropTypes from "prop-types";
+
+const TransitionLeft = (props) => <Slide {...props} direction="left" />;
 
 const Alert = forwardRef((props, ref) => (
   <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />
@@ -12,7 +15,13 @@ const Alert = forwardRef((props, ref) => (
 
 export default function DHSnackbar({ onClose, open, severity, message }) {
   return (
-    <Snackbar open={open} autoHideDuration={6000} onClose={onClose}>
+    <Snackbar
+      TransitionComponent={TransitionLeft}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      open={open}
+      autoHideDuration={6000}
+      onClose={onClose}
+    >
       <Alert onClose={message} severity={severity} sx={{ width: "100%" }}>
         {message}
       </Alert>
