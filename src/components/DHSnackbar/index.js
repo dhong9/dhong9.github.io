7d4cc1534f2d2f082457e2520b2f419ef1780415
dhong9 <danielhong35@gmail.com>
2023-05-13 13:37:21 -0700

    Created a snackbar component

---
+++ src/components/DHSnackbar/index.js
@@ -0,0 +1,36 @@
+import { forwardRef } from "react";
+
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+
+// prop-types is a library for typechecking of props
+import PropTypes from "prop-types";
+
+const Alert = forwardRef((props, ref) => (
+  <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />
+));
+
+export default function DHSnackbar({ onClose, open, severity, message }) {
+  return (
+    <Snackbar open={open} autoHideDuration={6000} onClose={onClose}>
+      <Alert onClose={message} severity={severity} sx={{ width: "100%" }}>
+        {message}
+      </Alert>
+    </Snackbar>
+  );
+}
+
+// Typechecking props of DHSnackbar
+DHSnackbar.propTypes = {
+  open: PropTypes.bool,
+  onClose: PropTypes.func,
+  severity: PropTypes.string,
+  message: PropTypes.string,
+};
+
+DHSnackbar.defaultProps = {
+  open: false,
+  onClose: null,
+  severity: "",
+  message: "",
+};
