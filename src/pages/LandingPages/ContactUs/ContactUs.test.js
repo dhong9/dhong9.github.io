709d238525ac78debaae23bb5e2b0c2ec3aea26f
dhong9 <danielhong35@gmail.com>
2023-05-28 20:27:47 -0700

    Added component render test for ContactUs

---
+++ src/pages/LandingPages/ContactUs/ContactUs.test.js
@@ -0,0 +1,17 @@
+import renderer from "react-test-renderer";
+import ContactUs from "pages/LandingPages/ContactUs";
+
+// Mocks
+jest.mock("components/MKBox", () => {
+    const { forwardRef } = jest.requireActual("react");
+    return {
+        __esModule: true,
+        default: forwardRef(() => <div>Mock Box</div>)
+    };
+});
+
+it("renders", () => {
+    const component = renderer.create(<ContactUs />);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+});
