6041cde32d38340d6851351229d9477bcfb28706
dhong9 <danielhong35@gmail.com>
2023-05-29 17:48:54 -0700

    Added useEffect mock to ContactUs

--- src/pages/LandingPages/ContactUs/ContactUs.test.js
+++ src/pages/LandingPages/ContactUs/ContactUs.test.js
@@ -1,3 +1,4 @@
+import React from "react";
 import renderer from "react-test-renderer";
 import ContactUs from "pages/LandingPages/ContactUs";
 
@@ -10,8 +11,21 @@ jest.mock("components/MKBox", () => {
     };
 });
 
-it("renders", () => {
-    const component = renderer.create(<ContactUs />);
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+describe("ContactUs", () => {
+    let useEffect;
+
+    const mockUseEffect = () => {
+        useEffect.mockImplementationOnce(f => f());
+    };
+
+    beforeEach(() => {
+        useEffect = jest.spyOn(React, "useEffect");
+        mockUseEffect();
+    });
+
+    it("renders", () => {
+        const component = renderer.create(<ContactUs />);
+        let tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
 });
