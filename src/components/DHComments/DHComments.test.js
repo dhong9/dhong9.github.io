e2b7d8bc41b746d1e884b28ac4f1a7991bdf2f09
dhong9 <danielhong35@gmail.com>
2023-06-07 20:08:01 -0700

    Unit test fix for DHComments

--- src/components/DHComments/DHComments.test.js
+++ src/components/DHComments/DHComments.test.js
@@ -24,7 +24,19 @@ describe("DHComments", () => {
     });
 
     it("renders", () => {
-        const component = renderer.create(<DHComments />);
+        const comments = [{
+            "id": 1,
+            "pageName": "connect4",
+            "name": "John Adams",
+            "email": "john_adams@aol.com",
+            "body": "Play connect 4 with me",
+            "create": "2023-05-26T17:42:43.263337Z",
+            "updated": "2023-05-26T17:42:43.263383Z",
+            "active": true,
+            "parent": null
+        }];
+        
+        const component = renderer.create(<DHComments comments={comments} />);
         let tree = component.toJSON();
         expect(tree).toMatchSnapshot();
     });
