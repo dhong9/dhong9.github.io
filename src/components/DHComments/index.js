d0e2c0677c232709239662839fdbda3e91aa0248
dhong9 <danielhong35@gmail.com>
2023-06-07 19:56:06 -0700

    Shows author and created date in Comments

--- src/components/DHComments/index.js
+++ src/components/DHComments/index.js
@@ -10,17 +10,15 @@ import Paper from "@mui/material/Paper";
 // prop-types is a library for typechecking of props
 import PropTypes from "prop-types";
 
-// https://dev.to/vigneshiyergithub/building-a-nested-comment-example-like-reddit-1o92
-
 export default function DHComments({ comments }) {
   return (
     <Paper style={{ padding: "40px 20px" }}>
-      {comments.map(({ id, body }) => (
+      {comments.map(({ id, body, name, create }) => (
         <Grid key={id} container wrap="nowrap" spacing={2}>
           <Grid justifyContent="left" item xs zeroMinWidth>
-            <h4 style={{ margin: 0, textAlign: "left" }}>Michel Michel</h4>
+            <h4 style={{ margin: 0, textAlign: "left" }}>{name}</h4>
             <p style={{ textAlign: "left" }}>{body}</p>
-            <p style={{ textAlign: "left", color: "gray" }}>posted 1 minute ago</p>
+            <p style={{ textAlign: "left", color: "gray" }}>created {create}</p>
           </Grid>
         </Grid>
       ))}
