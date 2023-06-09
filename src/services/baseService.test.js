d92f5ffbd85e07b2ae278b478414b15d94c26db1
dhong9 <danielhong35@gmail.com>
2023-06-09 11:12:23 -0700

    Axios test for base GET request

--- src/services/baseService.test.js
+++ src/services/baseService.test.js
@@ -1,62 +1,57 @@
 import axios from "axios";
-import MockAdapter from "axios-mock-adapter";
 import { createRequest, getRequest, postRequest } from "services/baseService";
 
-// This sets the mock adapter on the default instance
-var mock = new MockAdapter(axios);
-
-mock.onGet("/users").reply(200, {
-    users: [{ id: 1, name: "John Smith" }],
-});
-
-jest.mock("services/baseService", () => ({
-    getRequest: jest.fn(),
-    postRequest: jest.fn(),
-    createRequest: jest.fn()
-}));
+jest.mock('axios');
 
 describe("BaseService", () => {
+
+    const baseURL = "https://dhong9.pythonanywhere.com/";
+    const success = jest.fn();
+    const error = jest.fn();
+    const data = {};
+
+    beforeEach(() => {
+        axios.get.mockResolvedValue(data);
+    });
+    
     it("gets users", () => {
-        // Create success and error spy functions
-        const success = jest.fn();
-        const error = jest.fn();
 
         // Get users
         getRequest("users", success, error);
 
         // Verify that getRequest was called correctly
-        expect(getRequest).toHaveBeenCalledWith('users', success, error);
+        expect(axios.get).toHaveBeenCalledWith(baseURL + "users");
     });
 
-    it("posts a user", () => {
-        // Create success and error spy functions
-        const success = jest.fn();
-        const error = jest.fn();
+    // it("posts a user", () => {
+    //     // Create success and error spy functions
+    //     const success = jest.fn();
+    //     const error = jest.fn();
 
-        // Post user
-        const user = {
-            id: 2,
-            name: "Jane Doe"
-        };
-        postRequest("users", user, success, error);
+    //     // Post user
+    //     const user = {
+    //         id: 2,
+    //         name: "Jane Doe"
+    //     };
+    //     postRequest("users", user, success, error);
 
-        // Verify that getRequest was called correctly
-        expect(postRequest).toHaveBeenCalledWith('users', user, success, error);
-    });
+    //     // Verify that getRequest was called correctly
+    //     expect(postRequest).toHaveBeenCalledWith('users', user, success, error);
+    // });
 
-    it("creates a user", () => {
-        // Create success and error spy functions
-        const success = jest.fn();
-        const error = jest.fn();
+    // it("creates a user", () => {
+    //     // Create success and error spy functions
+    //     const success = jest.fn();
+    //     const error = jest.fn();
 
-        // Post user
-        const user = {
-            id: 2,
-            name: "Jane Doe"
-        };
-        createRequest("users", user, success, error);
+    //     // Post user
+    //     const user = {
+    //         id: 2,
+    //         name: "Jane Doe"
+    //     };
+    //     createRequest("users", user, success, error);
 
-        // Verify that getRequest was called correctly
-        expect(createRequest).toHaveBeenCalledWith('users', user, success, error);
-    });
+    //     // Verify that getRequest was called correctly
+    //     expect(createRequest).toHaveBeenCalledWith('users', user, success, error);
+    // });
 });
