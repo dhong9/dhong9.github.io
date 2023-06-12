132ce8dffdae36de88e3ff3e0826293576c044be
dhong9 <danielhong35@gmail.com>
2023-06-11 18:04:21 -0700

    Added render unit test for MKAlert

---
+++ src/components/MKAlert/MKAlert.test.js
@@ -0,0 +1,40 @@
+import renderer from "react-test-renderer";
+import MKAlert from "components/MKAlert";
+
+// Mocks
+jest.mock("components/MKBox", () => {
+    const { forwardRef } = jest.requireActual("react");
+    return {
+        __esModule: true,
+        default: forwardRef(() => <div>Mock Box</div>)
+    };
+});
+jest.mock("components/MKAlert/MKAlertRoot", () => {
+    const { forwardRef } = jest.requireActual("react");
+    return {
+        __esModule: true,
+        default: forwardRef(() => <div>MKAlertRoot</div>)
+    };
+});
+jest.mock("components/MKAlert/MKAlertCloseIcon", () => {
+    const { forwardRef } = jest.requireActual("react");
+    return {
+        __esModule: true,
+        default: forwardRef(() => <div>MKAlertCloseIcon</div>)
+    };
+});
+jest.mock("@mui/material/Fade", () => {
+    const { forwardRef } = jest.requireActual("react");
+    return {
+        __esModule: true,
+        default: forwardRef(() => <div>Fade</div>)
+    };
+});
+
+describe("MKAlert", () => {
+    it("renders", () => {
+        const component = renderer.create(<MKAlert />);
+        let tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
