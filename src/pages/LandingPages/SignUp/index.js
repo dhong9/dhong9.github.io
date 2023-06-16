66aaa15b9924e52b9d4a23c44019e8651b3acacc
dhong9 <danielhong35@gmail.com>
2023-06-16 10:02:05 -0700

    Removed download button from authentication pages

--- src/pages/LandingPages/SignUp/index.js
+++ src/pages/LandingPages/SignUp/index.js
@@ -106,17 +106,7 @@ function SignUpBasic() {
 
   return (
     <>
-      <DefaultNavbar
-        routes={routes}
-        action={{
-          type: "external",
-          route: "https://www.creative-tim.com/product/material-kit-react",
-          label: "free download",
-          color: "info",
-        }}
-        transparent
-        light
-      />
+      <DefaultNavbar routes={routes} transparent light />
       <DHSnackbar
         open={snackbarOpen}
         onClose={handleSnackbarClose}
