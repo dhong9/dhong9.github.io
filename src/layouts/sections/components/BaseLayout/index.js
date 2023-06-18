5adfe42bcff2ba6ad6610f1435f19667c06cd504
dhong9 <danielhong35@gmail.com>
2023-06-17 20:10:57 -0700

    Navbar on BaseLayout uses same layout as home screen

--- src/layouts/sections/components/BaseLayout/index.js
+++ src/layouts/sections/components/BaseLayout/index.js
@@ -13,8 +13,13 @@ Coded by www.creative-tim.com
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 
+import { useContext } from "react";
+
 // prop-types is a library for typechecking of props
 import PropTypes from "prop-types";
+
+// @mui material icons
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 // @mui material components
 import Container from "@mui/material/Container";
@@ -29,21 +34,50 @@ import DefaultNavbar from "examples/Navbars/DefaultNavbar";
 import CenteredFooter from "examples/Footers/CenteredFooter";
 import Breadcrumbs from "examples/Breadcrumbs";
 
+// Authentication
+import SignIn from "layouts/pages/authentication/sign-in";
+import SignUp from "layouts/pages/authentication/sign-up";
+import SignOut from "layouts/pages/authentication/sign-out";
+import AuthContext from "context/AuthContext";
+
 // Routes
 import routes from "routes";
 
 function BaseLayout({ breadcrumb, title, children }) {
+
+  let { user } = useContext(AuthContext);
+
+  const signedOutOptions = [
+    {
+      name: "sign in",
+      route: "/pages/authentication/sign-in",
+      component: <SignIn />,
+    },
+    {
+      name: "sign up",
+      route: "/pages/authentication/sign-up",
+      component: <SignUp />,
+    },
+  ];
+  const signedInOptions = [
+    {
+      name: "sign out",
+      route: "/pages/authentication/sign-out",
+      component: <SignOut />,
+    },
+  ];
+
+  const accountObj = {
+    name: user ? user.username : "Guest",
+    icon: <AccountCircleIcon />,
+    collapse: user ? signedInOptions : signedOutOptions,
+  };
+
   return (
     <MKBox display="flex" flexDirection="column" bgColor="white" minHeight="100vh">
       <MKBox bgColor="white" shadow="sm" py={0.25}>
         <DefaultNavbar
-          routes={routes}
-          action={{
-            type: "external",
-            route: "https://www.creative-tim.com/product/material-kit-react",
-            label: "free download",
-            color: "info",
-          }}
+          routes={[...routes, accountObj]}
           transparent
           relative
         />
