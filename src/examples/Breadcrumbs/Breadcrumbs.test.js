ec12ebb54afa48556baca82560b6b00e01dc8492
dhong9 <danielhong35@gmail.com>
2023-06-18 08:59:50 -0700

    Added unit test for Breadcrumbs

---
+++ src/examples/Breadcrumbs/Breadcrumbs.test.js
@@ -0,0 +1,37 @@
+import renderer from "react-test-renderer";
+import Breadcrumbs from "examples/Breadcrumbs";
+
+// Mocks
+jest.mock("components/MKBox", () => {
+    const { forwardRef } = jest.requireActual("react");
+    return {
+        __esModule: true,
+        default: forwardRef(() => <div>Mock Box</div>)
+    };
+});
+jest.mock("components/MKTypography", () => {
+    const { forwardRef } = jest.requireActual("react");
+    return {
+        __esModule: true,
+        default: forwardRef(() => <p>Mock Typography</p>)
+    };
+});
+jest.mock("@mui/material/Breadcrumbs", () => {
+    const { forwardRef } = jest.requireActual("react");
+    return {
+        __esModule: true,
+        default: forwardRef(() => <div>MUI Breadcrumbs</div>)
+    };
+});
+
+describe("BaseLayout", () => {
+    it("renders", () => {
+        const component = renderer.create(
+            <Breadcrumbs routes={[]}>
+                Test Breadcrumbs
+            </Breadcrumbs>
+        );
+        let tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
