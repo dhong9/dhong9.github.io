bc165383cc2c3922184b9c0c2f3e9250a7d83f6e
dhong9 <danielhong35@gmail.com>
2023-06-17 21:02:41 -0700

    Added unit test for BaseLayout

---
+++ src/layouts/sections/components/BaseLayout/BaseLayout.test.js
@@ -0,0 +1,68 @@
+import renderer from "react-test-renderer";
+import BaseLayout from "layouts/sections/components/BaseLayout";
+import React from "react";
+
+let realUseContext;
+let useContextMock;
+// Setup mock
+beforeEach(() => {
+    realUseContext = React.useContext;
+    useContextMock = React.useContext = jest.fn();
+});
+// Cleanup mock
+afterEach(() => {
+    React.useContext = realUseContext;
+});
+
+// Define Mocks
+jest.mock("components/MKBox", () => {
+    const { forwardRef } = jest.requireActual("react");
+    return {
+        __esModule: true,
+        default: forwardRef(() => <div>Mock Box</div>)
+    };
+});
+jest.mock("components/MKTypography", () => {
+    const { forwardRef } = jest.requireActual("react");
+    return {
+        __esModule: true,
+        default: forwardRef(() => <p>Mock Typography</p>)
+    };
+});
+jest.mock("examples/Breadcrumbs", () => {
+    const { forwardRef } = jest.requireActual("react");
+    return {
+        __esModule: true,
+        default: forwardRef(() => <div>Mock Breadcrumbs</div>)
+    };
+});
+jest.mock("react-monaco-editor", () => {
+    const { forwardRef } = jest.requireActual("react");
+    return {
+        __esModule: true,
+        default: forwardRef(() => <div>Mock Editor</div>)
+    };
+});
+jest.mock("context/AuthContext", () => {
+    const { createContext } = jest.requireActual('react'); 
+    return {
+        __esModule: true,
+        default: createContext()
+    }
+});
+
+describe("BaseLayout", () => {
+    it("BaseLayout", () => {
+        useContextMock.mockReturnValue("Test Value");
+        const component = renderer.create(
+            <BaseLayout
+                title="Test Layout"
+                breadcrumb={[]}
+            >
+                    Base
+            </BaseLayout>
+        );
+        let tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
