5ae07f7471e3b6e5f4a475c87f432e1b5b6af4b2
dhong9 <danielhong35@gmail.com>
2023-06-24 08:08:54 -0700

    Added unit test for PictureShuffle

---
+++ src/projects/games/pictureShuffle/PictureShuffle.test.js
@@ -0,0 +1,55 @@
+import renderer from "react-test-renderer";
+import PictureShuffle from "projects/games/pictureShuffle";
+import React from "react";
+
+let realUseContext;
+let useContextMock;
+// Setup mock
+beforeEach(() => {
+    realUseContext = React.useContext;
+    useContextMock = React.useContext = jest.fn();
+});
+// Cleanup mock
+afterEach(() => {
+    React.useContext = realUseContext;
+});
+
+
+// Define Mocks
+jest.mock("components/MKButton", () => {
+    const { forwardRef } = jest.requireActual("react");
+    return {
+        __esModule: true,
+        default: forwardRef(() => <button>Mock Box</button>)
+    };
+});
+jest.mock("components/MKBox", () => {
+    const { forwardRef } = jest.requireActual("react");
+    return {
+        __esModule: true,
+        default: forwardRef(() => <div>Mock Box</div>)
+    };
+});
+jest.mock("react-monaco-editor", () => {
+    const { forwardRef } = jest.requireActual("react");
+    return {
+        __esModule: true,
+        default: forwardRef(() => <div>Mock Editor</div>)
+    };
+});
+jest.mock("context/AuthContext", () => {
+    const { createContext } = jest.requireActual('react'); 
+    return {
+        __esModule: true,
+        default: createContext()
+    }
+});
+
+describe("PictureShuffle", () => {
+    it("renders", () => {
+        useContextMock.mockReturnValue("Test Value");
+        const component = renderer.create(<PictureShuffle />);
+        let tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
