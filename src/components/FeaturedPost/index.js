b48cce1aa32581343673c1f0edd997f46ec7c965
dhong9 <danielhong35@gmail.com>
2023-07-03 14:25:59 -0700

    Added sample featured post

--- src/components/FeaturedPost/index.js
+++ src/components/FeaturedPost/index.js
@@ -1,7 +1,57 @@
-export default function FeaturedPost() {
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardActionArea from "@mui/material/CardActionArea";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+
+// prop-types is a library for typechecking of props
+import PropTypes from "prop-types";
+
+export default function FeaturedPost(props) {
+  const { post } = props;
+
   return (
-    <div>
-      <h1>FeaturedPost</h1>
-    </div>
+    <Grid item xs={12} md={6}>
+      <CardActionArea component="a" href="#">
+        <Card sx={{ display: "flex" }}>
+          <CardContent sx={{ flex: 1 }}>
+            <Typography component="h2" variant="h5">
+              {post.title}
+            </Typography>
+            <Typography variant="subtitle1" color="text.secondary">
+              {post.date}
+            </Typography>
+            <Typography variant="subtitle1" paragraph>
+              {post.description}
+            </Typography>
+            <Typography variant="subtitle1" color="primary">
+              Continue reading...
+            </Typography>
+          </CardContent>
+          <CardMedia
+            component="img"
+            sx={{ width: 160, display: { xs: "none", sm: "block" } }}
+            image={post.image}
+            alt={post.imageLabel}
+          />
+        </Card>
+      </CardActionArea>
+    </Grid>
   );
 }
+
+// Typechecking props of DHSnackbar
+FeaturedPost.propTypes = {
+  post: PropTypes.shape({
+    date: PropTypes.string,
+    description: PropTypes.string,
+    image: PropTypes.string,
+    imageLabel: PropTypes.string,
+    title: PropTypes.string,
+  }),
+};
+
+FeaturedPost.defaultProps = {
+  post: { date: "", description: "", image: "", imageLabel: "", title: "" },
+};
--- src/layouts/pages/doughnut-rider/index.js
+++ src/layouts/pages/doughnut-rider/index.js
@@ -1,9 +1,17 @@
 import FeaturedPost from "components/FeaturedPost";
 
 export default function DoughnutRiderPage() {
+  const post = {
+    title: "Announcements",
+    date: "July 10, 2020",
+    description: "Making a big announcement",
+    image:
+      "https://images.pexels.com/photos/17353007/pexels-photo-17353007/free-photo-of-bikes.jpeg",
+    imageLabel: "Bikes",
+  };
   return (
     <div>
-      <FeaturedPost />
+      <FeaturedPost post={post} />
     </div>
   );
 }
