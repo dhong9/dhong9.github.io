3d32caf71d123f8ec3114baf79021d5c0c567bea
dhong9 <danielhong35@gmail.com>
2023-07-04 14:42:32 -0700

    Added MainFeaturedPost component

--- src/layouts/pages/doughnut-rider/index.js
+++ src/layouts/pages/doughnut-rider/index.js
@@ -1,6 +1,22 @@
+// @mui material components
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+
+// Page components
 import ForumHeader from "components/ForumHeader";
 import FeaturedPost from "components/FeaturedPost";
+import MainFeaturedPost from "components/MainFeaturedPost";
+
+const mainFeaturedPost = {
+  title: "Title of a longer featured blog post",
+  description:
+    "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
+  image: "https://source.unsplash.com/random",
+  imageText: "main image description",
+  linkText: "Continue reading…",
+};
 
 const theme = createTheme();
 
@@ -18,8 +34,16 @@ export default function DoughnutRiderPage() {
 
   return (
     <ThemeProvider theme={theme}>
-      <ForumHeader title="Forums Page" sections={sections} />
-      <FeaturedPost post={post} />
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <ForumHeader title="Forums Page" sections={sections} />
+        <main>
+          <MainFeaturedPost post={mainFeaturedPost} />
+          <Grid container spacing={4}>
+            <FeaturedPost post={post} />
+          </Grid>
+        </main>
+      </Container>
     </ThemeProvider>
   );
 }
