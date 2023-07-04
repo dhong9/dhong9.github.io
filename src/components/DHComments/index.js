32e729a47b44ceea7635e00cb5150fbf886f5b8f
dhong9 <danielhong35@gmail.com>
2023-07-04 15:28:08 -0700

    Updated comments component to be similar to FeaturedPost

--- src/components/DHComments/index.js
+++ src/components/DHComments/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 // // @mui material components
-import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
-
-// Material Kit 2 React components
-import MKTypography from "components/MKTypography";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardActionArea from "@mui/material/CardActionArea";
+import CardContent from "@mui/material/CardContent";
 
 // prop-types is a library for typechecking of props
 import PropTypes from "prop-types";
@@ -15,16 +16,25 @@ export default function DHComments({ comments }) {
     <Paper style={{ padding: "40px 20px" }}>
       {comments.map(({ id, body, name, create }) => (
         <Grid key={id} container wrap="nowrap" spacing={2}>
-          <MKTypography variant="h4" sx={{ margin: 0, textAlign: "left" }}>
-            {name}
-          </MKTypography>
-          <Grid justifyContent="left" item xs zeroMinWidth>
-            <MKTypography variant="subtitle1" color="text.secondary">
-              created {new Date(create).toLocaleString()}
-            </MKTypography>
-            <MKTypography variant="subtitle1" paragraph>
-              {body}
-            </MKTypography>
+          <Grid item xs={12} md={6}>
+            <CardActionArea component="a" href="#">
+              <Card sx={{ display: "flex" }}>
+                <CardContent sx={{ flex: 1 }}>
+                  <Typography component="h2" variant="h5">
+                    {name}
+                  </Typography>
+                  <Typography variant="subtitle1" color="text.secondary">
+                    {new Date(create).toLocaleString()}
+                  </Typography>
+                  <Typography variant="subtitle1" paragraph>
+                    {body}
+                  </Typography>
+                  <Typography variant="subtitle1" color="primary">
+                    Reply...
+                  </Typography>
+                </CardContent>
+              </Card>
+            </CardActionArea>
           </Grid>
         </Grid>
       ))}
