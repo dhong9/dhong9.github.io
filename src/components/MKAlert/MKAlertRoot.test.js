1acdb2edb132a221aa11563d78d1702483e2732c
dhong9 <danielhong35@gmail.com>
2023-07-04 21:29:09 -0700

    Added unit test for MKAlertRoot with info color

--- src/components/MKAlert/MKAlertRoot.test.js
+++ src/components/MKAlert/MKAlertRoot.test.js
@@ -1,43 +1,86 @@
-import renderer from 'react-test-renderer';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import MKAlertRoot from 'components/MKAlert/MKAlertRoot';
+import renderer from "react-test-renderer";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import MKAlertRoot from "components/MKAlert/MKAlertRoot";
 import pxToRem from "assets/theme/functions/pxToRem";
 import linearGradient from "assets/theme/functions/linearGradient";
 
-describe('MKAlertRoot', () => {
-    it('renders', () => {
-        // Create a dummy theme object with the mock pxToRem
-        const theme = createTheme({
-            palette: {
-                white: {
-                    main: "#00F"
-                },
-                gradients: {
-                    "#FF00FF": "#FF00FF"
-                }
-            },
-            typography: {
-                size: {
-                    xl: "16pt"
-                },
-                fontWeightMedium: 500
-            },
-            borders: {
-                borderRadius: 10
-            },
-            functions: { pxToRem, linearGradient }
-        });
-        const ownerState = {
-            color: "#FF00FF"
-        };
-
-        const component = renderer.create(
-            <ThemeProvider theme={theme}>
-                <MKAlertRoot onClick={jest.fn()} ownerState={ownerState}>Hello!</MKAlertRoot>
-            </ThemeProvider>
-        );
-
-        let tree = component.toJSON();
-        expect(tree).toMatchSnapshot();
+describe("MKAlertRoot", () => {
+  it("renders with custom color", () => {
+    // Create a dummy theme object with the mock pxToRem
+    const theme = createTheme({
+      palette: {
+        white: {
+          main: "#00F",
+        },
+        gradients: {
+          "#FF00FF": "#FF00FF",
+        },
+      },
+      typography: {
+        size: {
+          xl: "16pt",
+        },
+        fontWeightMedium: 500,
+      },
+      borders: {
+        borderRadius: 10,
+      },
+      functions: { pxToRem, linearGradient },
     });
+    const ownerState = {
+      color: "#FF00FF",
+    };
+
+    const component = renderer.create(
+      <ThemeProvider theme={theme}>
+        <MKAlertRoot onClick={jest.fn()} ownerState={ownerState}>
+          Hello!
+        </MKAlertRoot>
+      </ThemeProvider>
+    );
+
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("renders without custom color", () => {
+    // Create a dummy theme object with the mock pxToRem
+    const theme = createTheme({
+      palette: {
+        white: {
+          main: "#00F",
+        },
+        gradients: {
+          info: {
+            main: "#1A73E8",
+            focus: "#1662C4",
+          },
+        },
+      },
+      typography: {
+        size: {
+          xl: "16pt",
+        },
+        fontWeightMedium: 500,
+      },
+      borders: {
+        borderRadius: 10,
+      },
+      functions: { pxToRem, linearGradient },
+    });
+    const ownerState = {
+      color: "#FF00FF",
+    };
+
+    const component = renderer.create(
+      <ThemeProvider theme={theme}>
+        <MKAlertRoot onClick={jest.fn()} ownerState={ownerState}>
+          Hello!
+        </MKAlertRoot>
+      </ThemeProvider>
+    );
+
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
