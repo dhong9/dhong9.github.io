b33a2ec9474d46e5ce7659ecdb335339de9540f6
dhong9 <danielhong35@gmail.com>
2023-07-04 21:18:55 -0700

    Updated DHSnackbar test to render Alert component

--- src/components/DHSnackbar/DHSnackbar.test.js
+++ src/components/DHSnackbar/DHSnackbar.test.js
@@ -3,34 +3,43 @@ import DHSnackbar from "components/DHSnackbar";
 
 // Mocks
 jest.mock("@mui/material/Snackbar", () => {
-    const { forwardRef } = jest.requireActual("react");
-    return {
-        __esModule: true,
-        default: forwardRef(() => <div>Mock Snackbar</div>)
-    };
+  const { forwardRef } = jest.requireActual("react");
+  const Snackbar = forwardRef((props, ref) => (
+    <div ref={ref}>
+      Mock Snackbar
+      {props.children}
+    </div>
+  ));
+  return {
+    __esModule: true,
+    default: Snackbar,
+  };
 });
 jest.mock("@mui/material/Alert", () => {
-    const { forwardRef } = jest.requireActual("react");
-    return {
-        __esModule: true,
-        default: forwardRef(() => <div>Mock Alert</div>)
-    };
+  const { forwardRef } = jest.requireActual("react");
+  const Alert = forwardRef((props, ref) => (
+    <div ref={ref}>
+      Mock Alert
+      {props.children}
+    </div>
+  ));
+  return {
+    __esModule: true,
+    default: Alert,
+  };
 });
 jest.mock("@mui/material/Slide", () => {
-    const { forwardRef } = jest.requireActual("react");
-    return {
-        __esModule: true,
-        default: forwardRef(() => <div>Mock Slide</div>)
-    };
+  const { forwardRef } = jest.requireActual("react");
+  return {
+    __esModule: true,
+    default: forwardRef(() => <div>Mock Slide</div>),
+  };
 });
 
 describe("DHSnackbar", () => {
-
-    it("renders", () => {
-        const component = renderer.create(<DHSnackbar />);
-        let tree = component.toJSON();
-        expect(tree).toMatchSnapshot();
-    });
-
-
+  it("renders", () => {
+    const component = renderer.create(<DHSnackbar />);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
