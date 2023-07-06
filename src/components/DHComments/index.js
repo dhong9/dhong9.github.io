b8f76e85dd35834221ada403cb4f0abf8fe86fb7
dhong9 <danielhong35@gmail.com>
2023-07-06 13:04:08 -0700

    Added ReactMarkdown parsing to comments component

--- src/components/DHComments/index.js
+++ src/components/DHComments/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-// // @mui material components
+// @mui material components
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
@@ -11,10 +11,12 @@ import CardContent from "@mui/material/CardContent";
 // prop-types is a library for typechecking of props
 import PropTypes from "prop-types";
 
+import ReactMarkdown from "react-markdown";
+
 export default function DHComments({ comments }) {
   return (
     <Paper style={{ padding: "40px 20px" }}>
-      {comments.map(({ id, body, name, create }) => (
+      {comments.map(({ id, body, name, create, isPlainText }) => (
         <Grid key={id} item xs={12} md={6}>
           <CardActionArea component="a" href="#">
             <Card sx={{ display: "flex" }}>
@@ -26,7 +28,7 @@ export default function DHComments({ comments }) {
                   {new Date(create).toLocaleString()}
                 </Typography>
                 <Typography variant="subtitle1" paragraph>
-                  {body}
+                  {isPlainText ? body : <ReactMarkdown>{body}</ReactMarkdown>}
                 </Typography>
                 <Typography variant="subtitle1" color="primary">
                   Reply...
