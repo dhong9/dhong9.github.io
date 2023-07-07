d561a0e5cd77f77815ed7ed6c1eb17a010c05291
dhong9 <danielhong35@gmail.com>
2023-07-07 14:18:26 -0700

    Added unit test fof input change in Contact form

--- src/pages/LandingPages/ContactUs/ContactUs.test.js
+++ src/pages/LandingPages/ContactUs/ContactUs.test.js
@@ -1,4 +1,5 @@
 import renderer from "react-test-renderer";
+import { render, fireEvent } from "@testing-library/react";
 import ContactUs from "pages/LandingPages/ContactUs";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import linearGradient from "assets/theme/functions/linearGradient";
@@ -13,83 +14,83 @@ jest.mock("@mui/base/TextareaAutosize", () => {
 });
 
 describe("ContactUs", () => {
+  // Create a dummy theme object
+  const theme = createTheme({
+    palette: {
+      white: {
+        main: "#00F",
+      },
+      gradients: {
+        primary: {
+          main: "#EC407A",
+          state: "#D81B60",
+        },
+
+        secondary: {
+          main: "#747b8a",
+          state: "#495361",
+        },
+
+        info: {
+          main: "#49a3f1",
+          state: "#1A73E8",
+        },
+
+        success: {
+          main: "#66BB6A",
+          state: "#43A047",
+        },
+
+        warning: {
+          main: "#FFA726",
+          state: "#FB8C00",
+        },
+
+        error: {
+          main: "#EF5350",
+          state: "#E53935",
+        },
+
+        light: {
+          main: "#EBEFF4",
+          state: "#CED4DA",
+        },
+
+        dark: {
+          main: "#42424a",
+          state: "#191919",
+        },
+      },
+      transparent: {
+        main: "transparent",
+      },
+    },
+    typography: {
+      size: {
+        xl: "16pt",
+      },
+      fontWeightMedium: 500,
+    },
+    borders: {
+      borderRadius: {
+        xs: pxToRem(1.6),
+        sm: pxToRem(2),
+        md: pxToRem(6),
+        lg: pxToRem(8),
+        xl: pxToRem(12),
+        xxl: pxToRem(16),
+        section: pxToRem(160),
+      },
+    },
+    boxShadows: {
+      colored: {
+        "#FF00FF": "#FF00FF",
+      },
+    },
+    functions: { pxToRem, linearGradient },
+  });
+
   it("renders", () => {
-    // Create a dummy theme object with the mock pxToRem
-    const theme = createTheme({
-      palette: {
-        white: {
-          main: "#00F",
-        },
-        gradients: {
-          primary: {
-            main: "#EC407A",
-            state: "#D81B60",
-          },
-
-          secondary: {
-            main: "#747b8a",
-            state: "#495361",
-          },
-
-          info: {
-            main: "#49a3f1",
-            state: "#1A73E8",
-          },
-
-          success: {
-            main: "#66BB6A",
-            state: "#43A047",
-          },
-
-          warning: {
-            main: "#FFA726",
-            state: "#FB8C00",
-          },
-
-          error: {
-            main: "#EF5350",
-            state: "#E53935",
-          },
-
-          light: {
-            main: "#EBEFF4",
-            state: "#CED4DA",
-          },
-
-          dark: {
-            main: "#42424a",
-            state: "#191919",
-          },
-        },
-        transparent: {
-          main: "transparent",
-        },
-      },
-      typography: {
-        size: {
-          xl: "16pt",
-        },
-        fontWeightMedium: 500,
-      },
-      borders: {
-        borderRadius: {
-          xs: pxToRem(1.6),
-          sm: pxToRem(2),
-          md: pxToRem(6),
-          lg: pxToRem(8),
-          xl: pxToRem(12),
-          xxl: pxToRem(16),
-          section: pxToRem(160),
-        },
-      },
-      boxShadows: {
-        colored: {
-          "#FF00FF": "#FF00FF",
-        },
-      },
-      functions: { pxToRem, linearGradient },
-    });
-
     const component = renderer.create(
       <ThemeProvider theme={theme}>
         <ContactUs />
@@ -98,5 +99,20 @@ describe("ContactUs", () => {
 
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it("should handle input change", () => {
+    const { getByLabelText } = render(
+      <ThemeProvider theme={theme}>
+        <ContactUs />
+      </ThemeProvider>
+    );
+    const fullNameInput = getByLabelText("Full Name");
+
+    // Simulate the onChange event
+    fireEvent.change(fullNameInput, { target: { value: "Johnny Appleseed" } });
+
+    // Assert that the state or props have been updated accordingly
+    expect(fullNameInput.value).toEqual("Johnny Appleseed");
   });
 });
