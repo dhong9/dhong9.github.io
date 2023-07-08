633a0783ae9c3f253aa0a701dfe17ed19e321561
dhong9 <danielhong35@gmail.com>
2023-07-07 20:58:45 -0700

    Set textGradient to true when checking out info text

--- src/components/MKTypography/MKTypographyRoot.test.js
+++ src/components/MKTypography/MKTypographyRoot.test.js
@@ -2,49 +2,49 @@ import renderer from "react-test-renderer";
 import MKTypographyRoot from "components/MKTypography/MKTypographyRoot";
 
 // Material Kit 2 React themes
-import { ThemeProvider } from '@mui/material/styles';
+import { ThemeProvider } from "@mui/material/styles";
 import theme from "assets/theme";
 
 describe("MKTypographyRoot", () => {
-    it("renders with text color", () => {
-        const component = renderer.create(
-            <ThemeProvider theme={theme}>
-                <MKTypographyRoot
-                ownerState={{
-                    color: "text",
-                    textTransform: "capitalize",
-                    verticalAlign: "middle",
-                    fontWeight: "bold",
-                    opacity: 1,
-                    textGradient: false,
-                }}
-                >
-                    Test Typography
-                </MKTypographyRoot>
-            </ThemeProvider>
-        );
-        let tree = component.toJSON();
-        expect(tree).toMatchSnapshot();
-    });
+  it("renders with text color", () => {
+    const component = renderer.create(
+      <ThemeProvider theme={theme}>
+        <MKTypographyRoot
+          ownerState={{
+            color: "text",
+            textTransform: "capitalize",
+            verticalAlign: "middle",
+            fontWeight: "bold",
+            opacity: 1,
+            textGradient: false,
+          }}
+        >
+          Test Typography
+        </MKTypographyRoot>
+      </ThemeProvider>
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 
-    it("renders with info color", () => {
-        const component = renderer.create(
-            <ThemeProvider theme={theme}>
-                <MKTypographyRoot
-                ownerState={{
-                    color: "info",
-                    textTransform: "capitalize",
-                    verticalAlign: "middle",
-                    fontWeight: "bold",
-                    opacity: 1,
-                    textGradient: false,
-                }}
-                >
-                    Test Typography
-                </MKTypographyRoot>
-            </ThemeProvider>
-        );
-        let tree = component.toJSON();
-        expect(tree).toMatchSnapshot();
-    });
+  it("renders with info color", () => {
+    const component = renderer.create(
+      <ThemeProvider theme={theme}>
+        <MKTypographyRoot
+          ownerState={{
+            color: "info",
+            textTransform: "capitalize",
+            verticalAlign: "middle",
+            fontWeight: "bold",
+            opacity: 1,
+            textGradient: true,
+          }}
+        >
+          Test Typography
+        </MKTypographyRoot>
+      </ThemeProvider>
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
