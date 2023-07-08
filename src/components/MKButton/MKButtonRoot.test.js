02ace28954c289b034ab96a3969da4fc4fe5a1c2
dhong9 <danielhong35@gmail.com>
2023-07-07 21:16:13 -0700

    Added hover test to MKButtonRoot

--- src/components/MKButton/MKButtonRoot.test.js
+++ src/components/MKButton/MKButtonRoot.test.js
@@ -1,23 +1,46 @@
-import renderer from 'react-test-renderer';
-import MKButtonRoot from 'components/MKButton/MKButtonRoot';
+// React testing libraries
+import renderer from "react-test-renderer";
+import { render, fireEvent } from "@testing-library/react";
+
+// Component to test
+import MKButtonRoot from "components/MKButton/MKButtonRoot";
 
 // Material Kit 2 React themes
-import { ThemeProvider } from '@mui/material/styles';
+import { ThemeProvider } from "@mui/material/styles";
 import theme from "assets/theme";
 
-describe('MKButtonRoot', () => {
-    it('renders', () => {
-        const ownerState = {
-            color: "#FF00FF"
-        };
+describe("MKButtonRoot", () => {
+  it("renders", () => {
+    const ownerState = {
+      color: "#FF00FF",
+    };
 
-        const component = renderer.create(
-            <ThemeProvider theme={theme}>
-                <MKButtonRoot onClick={jest.fn()} ownerState={ownerState}>Hello!</MKButtonRoot>
-            </ThemeProvider>
-        );
+    const component = renderer.create(
+      <ThemeProvider theme={theme}>
+        <MKButtonRoot onClick={jest.fn()} ownerState={ownerState}>
+          Hello!
+        </MKButtonRoot>
+      </ThemeProvider>
+    );
 
-        let tree = component.toJSON();
-        expect(tree).toMatchSnapshot();
-    });
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("simulates hover over the button", () => {
+    const ownerState = {
+      color: "#FF00FF",
+    };
+
+    const { container, getByText } = render(
+        <ThemeProvider theme={theme}>
+            <MKButtonRoot onClick={jest.fn()} ownerState={ownerState}>Hello!</MKButtonRoot>
+        </ThemeProvider>
+    );
+
+    const button = getByText("Hello!");
+    fireEvent.mouseEnter(button); // Simulate mouse hover over the button
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
 });
