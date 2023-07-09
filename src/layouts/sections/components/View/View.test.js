bf3603ae2f0e685dbeb144d321ee4621fca9fab6
dhong9 <danielhong35@gmail.com>
2023-07-08 21:54:48 -0700

    Added button click test to View component

--- src/layouts/sections/components/View/View.test.js
+++ src/layouts/sections/components/View/View.test.js
@@ -1,5 +1,6 @@
-// React testing libraries
+// Unit test libraries
 import renderer from "react-test-renderer";
+import { render, fireEvent } from "@testing-library/react";
 
 // Material Kit 2 React themes
 import { ThemeProvider } from "@mui/material/styles";
@@ -19,5 +20,18 @@ describe("View", () => {
     );
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it("sets success", () => {
+    const { container, getByText } = render(
+      <ThemeProvider theme={theme}>
+        <View title="Test View" code="" height="100%">
+          Test View
+        </View>
+      </ThemeProvider>
+    );
+    const copyButton = getByText("Copy");
+    fireEvent.click(copyButton);
+    expect(container).toMatchSnapshot();
   });
 });
