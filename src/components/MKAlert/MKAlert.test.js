9d9d8f67039db7628a8ca8650776d2369ec92d6c
dhong9 <danielhong35@gmail.com>
2023-07-09 15:17:36 -0700

    Dismisses Alert in unit test

--- src/components/MKAlert/MKAlert.test.js
+++ src/components/MKAlert/MKAlert.test.js
@@ -1,46 +1,48 @@
+// React testing libraries
 import renderer from "react-test-renderer";
+import { render, fireEvent } from "@testing-library/react";
+
+// Material Kit 2 React themes
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "assets/theme";
+
+// Component to test
 import MKAlert from "components/MKAlert";
 
 // Mocks
-jest.mock("components/MKBox", () => {
-    const { forwardRef } = jest.requireActual("react");
-    return {
-        __esModule: true,
-        default: forwardRef(() => <div>Mock Box</div>)
-    };
-});
-jest.mock("components/MKAlert/MKAlertRoot", () => {
-    const { forwardRef } = jest.requireActual("react");
-    return {
-        __esModule: true,
-        default: forwardRef(() => <div>MKAlertRoot</div>)
-    };
-});
-jest.mock("components/MKAlert/MKAlertCloseIcon", () => {
-    const { forwardRef } = jest.requireActual("react");
-    return {
-        __esModule: true,
-        default: forwardRef(() => <div>MKAlertCloseIcon</div>)
-    };
-});
 jest.mock("@mui/material/Fade", () => {
     const { forwardRef } = jest.requireActual("react");
+    const Fade = forwardRef((props, ref) => (
+      <div ref={ref}>
+        Mock Fade
+        {props.children}
+      </div>
+    ));
     return {
-        __esModule: true,
-        default: forwardRef(() => <div>Fade</div>)
+      __esModule: true,
+      default: Fade,
     };
-});
+  });
 
 describe("MKAlert", () => {
-    it("renders", () => {
-        const component = renderer.create(<MKAlert>Hello!</MKAlert>);
-        let tree = component.toJSON();
-        expect(tree).toMatchSnapshot();
-    });
+  it("renders", () => {
+    const component = renderer.create(
+      <ThemeProvider theme={theme}>
+        <MKAlert>Hello!</MKAlert>
+      </ThemeProvider>
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 
-    it("renders dismissible", () => {
-        const component = renderer.create(<MKAlert dismissible={true}>Dismissible</MKAlert>);
-        let tree = component.toJSON();
-        expect(tree).toMatchSnapshot();
-    });
+  it("can be dismissed", () => {
+    const { container, getByText } = render(
+      <ThemeProvider theme={theme}>
+        <MKAlert dismissible={true}>Dismissible</MKAlert>
+      </ThemeProvider>
+    );
+    const dismissButton = getByText("\u00D7");
+    fireEvent.click(dismissButton);
+    expect(container).toMatchSnapshot();
+  });
 });
