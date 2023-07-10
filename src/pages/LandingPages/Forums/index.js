8d393f087061eda828c89e2a564ae786117f1417
dhong9 <danielhong35@gmail.com>
2023-07-09 20:34:22 -0700

    Used BaseLayout to define forum page

--- src/pages/LandingPages/Forums/index.js
+++ src/pages/LandingPages/Forums/index.js
@@ -1,33 +1,13 @@
-import { useContext, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
-// @mui material icons
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-
-// Material Kit 2 React example components
-import DefaultNavbar from "examples/Navbars/DefaultNavbar";
-import CenteredFooter from "examples/Footers/CenteredFooter";
-
-// Material Kit 2 React components
-import MKBox from "components/MKBox";
-
-// Material Kit 2 React page layout routes
-import routes from "routes";
-
-// Authentication
-import SignIn from "layouts/pages/authentication/sign-in";
-import SignUp from "layouts/pages/authentication/sign-up";
-import SignOut from "layouts/pages/authentication/sign-out";
-import AuthContext from "context/AuthContext";
-
-// Forum components
+// Sections components
+import BaseLayout from "layouts/sections/components/BaseLayout";
 import ForumHeader from "components/ForumHeader";
 
 // Categories service
 import { getCategories } from "services/categoriesService";
 
 function Forums() {
-  let { user } = useContext(AuthContext);
-
   const [categories, setCategories] = useState([]);
 
   // Get categories on page load
@@ -37,39 +17,13 @@ function Forums() {
     });
   }, []);
 
-  const signedOutOptions = [
-    {
-      name: "sign in",
-      route: "/pages/authentication/sign-in",
-      component: <SignIn />,
-    },
-    {
-      name: "sign up",
-      route: "/pages/authentication/sign-up",
-      component: <SignUp />,
-    },
-  ];
-  const signedInOptions = [
-    {
-      name: "sign out",
-      route: "/pages/authentication/sign-out",
-      component: <SignOut />,
-    },
-  ];
-  const accountObj = {
-    name: user ? user.username : "Guest",
-    icon: <AccountCircleIcon />,
-    collapse: user ? signedInOptions : signedOutOptions,
-  };
-
   return (
-    <>
-      <DefaultNavbar routes={[...routes, accountObj]} />
+    <BaseLayout
+      title="Doughnut Rider"
+      breadcrumb={[{ label: "Doughnut Rider", route: "/doughnutRider" }]}
+    >
       <ForumHeader categories={categories} />
-      <MKBox mt="auto">
-        <CenteredFooter />
-      </MKBox>
-    </>
+    </BaseLayout>
   );
 }
 
