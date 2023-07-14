0b290d099ce79a988e1e5ab9b4f2d48127ce5c87
dhong9 <danielhong35@gmail.com>
2023-07-13 21:48:46 -0700

    Removed component mocks from DHSnackbar

--- src/components/DHSnackbar/DHSnackbar.test.js
+++ src/components/DHSnackbar/DHSnackbar.test.js
@@ -1,40 +1,8 @@
+// React testing libraries
 import renderer from "react-test-renderer";
-import DHSnackbar from "components/DHSnackbar";
 
-// Mocks
-jest.mock("@mui/material/Snackbar", () => {
-  const { forwardRef } = jest.requireActual("react");
-  const Snackbar = forwardRef((props, ref) => (
-    <div ref={ref}>
-      Mock Snackbar
-      {props.children}
-    </div>
-  ));
-  return {
-    __esModule: true,
-    default: Snackbar,
-  };
-});
-jest.mock("@mui/material/Alert", () => {
-  const { forwardRef } = jest.requireActual("react");
-  const Alert = forwardRef((props, ref) => (
-    <div ref={ref}>
-      Mock Alert
-      {props.children}
-    </div>
-  ));
-  return {
-    __esModule: true,
-    default: Alert,
-  };
-});
-jest.mock("@mui/material/Slide", () => {
-  const { forwardRef } = jest.requireActual("react");
-  return {
-    __esModule: true,
-    default: forwardRef(() => <div>Mock Slide</div>),
-  };
-});
+// Component to test
+import DHSnackbar from "components/DHSnackbar";
 
 describe("DHSnackbar", () => {
   it("renders", () => {
