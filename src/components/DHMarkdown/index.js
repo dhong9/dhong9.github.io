fb5c029e4a456247d74e9788fa1864e4552dbcbc
dhong9 <danielhong35@gmail.com>
2023-07-13 21:42:14 -0700

    Replaced material components in Markdown with material kit

--- src/components/DHMarkdown/index.js
+++ src/components/DHMarkdown/index.js
@@ -1,18 +1,24 @@
 import ReactMarkdown from "react-markdown";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
+
+// Material Kit 2 React components
+import MKBox from "components/MKBox";
+import MKTypography from "components/MKTypography";
 
 const components = {
   components: {
-    h1: ({ ...props }) => <Typography gutterBottom={true} variant="h4" component="h1" {...props} />,
-    h2: ({ ...props }) => <Typography gutterBottom={true} variant="h6" component="h2" {...props} />,
-    h3: ({ ...props }) => <Typography gutterBottom={true} variant="subtitle1" {...props} />,
-    h4: ({ ...props }) => (
-      <Typography gutterBottom={true} variant="caption" paragraph={true} {...props} />
+    h1: ({ ...props }) => (
+      <MKTypography gutterBottom={true} variant="h4" component="h1" {...props} />
     ),
-    p: ({ ...props }) => <Typography paragraph={true} {...props} />,
+    h2: ({ ...props }) => (
+      <MKTypography gutterBottom={true} variant="h6" component="h2" {...props} />
+    ),
+    h3: ({ ...props }) => <MKTypography gutterBottom={true} variant="subtitle1" {...props} />,
+    h4: ({ ...props }) => (
+      <MKTypography gutterBottom={true} variant="caption" paragraph={true} {...props} />
+    ),
+    p: ({ ...props }) => <MKTypography paragraph={true} {...props} />,
     a: "@mui/material/Link",
-    li: ({ ...props }) => <Box component="li" sx={{ mt: 1, typography: "body1" }} {...props} />,
+    li: ({ ...props }) => <MKBox component="li" sx={{ mt: 1, typography: "body1" }} {...props} />,
   },
 };
 
