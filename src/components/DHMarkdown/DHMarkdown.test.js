f0ff29080199b376e094ba24c8517566c1ba7df4
dhong9 <danielhong35@gmail.com>
2023-07-14 15:55:54 -0700

    Renders all markdown components in markdown component test

--- src/components/DHMarkdown/DHMarkdown.test.js
+++ src/components/DHMarkdown/DHMarkdown.test.js
@@ -5,13 +5,38 @@ import renderer from "react-test-renderer";
 import DHMarkdown from "components/DHMarkdown";
 
 describe("DHMarkdown", () => {
-  it("renders", () => {
-    const component = renderer.create(<DHMarkdown>
-      # Hello *world*!
-      ## First programming sample
-      ### Using main
-      #### Arguments
-    </DHMarkdown>);
+  it("renders h1 component", () => {
+    const component = renderer.create(<DHMarkdown># Hello world!</DHMarkdown>);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("renders h2 component", () => {
+    const component = renderer.create(<DHMarkdown>## Hello world!</DHMarkdown>);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("renders h3 component", () => {
+    const component = renderer.create(<DHMarkdown>### Hello world!</DHMarkdown>);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("renders h4 component", () => {
+    const component = renderer.create(<DHMarkdown>#### Hello world!</DHMarkdown>);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("renders p component", () => {
+    const component = renderer.create(<DHMarkdown>Hello world!</DHMarkdown>);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("renders a component", () => {
+    const component = renderer.create(<DHMarkdown>[Hello world](https://example.com)</DHMarkdown>);
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
