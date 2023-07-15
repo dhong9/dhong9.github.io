abd024f8319d1f3efa84235d1dd879947389ea81
dhong9 <danielhong35@gmail.com>
2023-07-14 17:04:19 -0700

    Added component level unit test to Markdown component

--- src/components/DHMarkdown/DHMarkdown.test.js
+++ src/components/DHMarkdown/DHMarkdown.test.js
@@ -1,20 +1,56 @@
 // React testing libraries
 import renderer from "react-test-renderer";
 
+// Mock the MKTypography component
+import MKTypography from "components/MKTypography";
+jest.mock("components/MKTypography", () => {
+  return {
+    __esModule: true,
+    default: jest.fn().mockImplementation((props) => {
+      const { children } = props;
+      return <div {...props}>{children}</div>;
+    }),
+  };
+});
+
 // Component to test
 import DHMarkdown from "components/DHMarkdown";
 
 describe("DHMarkdown", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it("renders h1 component", () => {
     const component = renderer.create(<DHMarkdown># Hello world!</DHMarkdown>);
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
+
+    expect(MKTypography).toHaveBeenCalledWith(
+      expect.objectContaining({
+        gutterBottom: true,
+        variant: "h4",
+        component: "h1",
+        children: ["Hello world!"],
+      }),
+      {}
+    );
   });
 
   it("renders h2 component", () => {
     const component = renderer.create(<DHMarkdown>## Hello world!</DHMarkdown>);
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
+
+    expect(MKTypography).toHaveBeenCalledWith(
+      expect.objectContaining({
+        gutterBottom: true,
+        variant: "h6",
+        component: "h2",
+        children: ["Hello world!"],
+      }),
+      {}
+    );
   });
 
   it("renders h3 component", () => {
--- src/components/DHMarkdown/index.js
+++ src/components/DHMarkdown/index.js
@@ -1,25 +1,44 @@
 import ReactMarkdown from "react-markdown";
 
+// MUI components
+import Link from "@mui/material/Link";
+
 // Material Kit 2 React components
 import MKBox from "components/MKBox";
 import MKTypography from "components/MKTypography";
 
 const components = {
-  components: {
-    h1: ({ ...props }) => (
-      <MKTypography gutterBottom={true} variant="h4" component="h1" {...props} />
-    ),
-    h2: ({ ...props }) => (
-      <MKTypography gutterBottom={true} variant="h6" component="h2" {...props} />
-    ),
-    h3: ({ ...props }) => <MKTypography gutterBottom={true} variant="subtitle1" {...props} />,
-    h4: ({ ...props }) => (
-      <MKTypography gutterBottom={true} variant="caption" paragraph={true} {...props} />
-    ),
-    p: ({ ...props }) => <MKTypography paragraph={true} {...props} />,
-    a: "@mui/material/Link",
-    li: ({ ...props }) => <MKBox component="li" sx={{ mt: 1, typography: "body1" }} {...props} />,
-  },
+  h1: ({ children, ...props }) => (
+    <MKTypography gutterBottom variant="h4" component="h1" {...props}>
+      {children}
+    </MKTypography>
+  ),
+  h2: ({ children, ...props }) => (
+    <MKTypography gutterBottom variant="h6" component="h2" {...props}>
+      {children}
+    </MKTypography>
+  ),
+  h3: ({ children, ...props }) => (
+    <MKTypography gutterBottom variant="subtitle1" {...props}>
+      {children}
+    </MKTypography>
+  ),
+  h4: ({ children, ...props }) => (
+    <MKTypography gutterBottom variant="caption" paragraph {...props}>
+      {children}
+    </MKTypography>
+  ),
+  p: ({ children, ...props }) => (
+    <MKTypography paragraph {...props}>
+      {children}
+    </MKTypography>
+  ),
+  a: ({ children, ...props }) => <Link {...props}>{children}</Link>,
+  li: ({ children, ...props }) => (
+    <MKBox component="li" sx={{ mt: 1, typography: "body1" }} {...props}>
+      {children}
+    </MKBox>
+  ),
 };
 
 export default function DHMarkdown(props) {
