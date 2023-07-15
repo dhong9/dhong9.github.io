9f1c3f7de9272a7b6fef3c1a60bb880ffb58032b
dhong9 <danielhong35@gmail.com>
2023-07-15 16:43:50 -0700

    Removed mock AuthContext component

--- src/pages/LandingPages/SignIn/SignIn.test.js
+++ src/pages/LandingPages/SignIn/SignIn.test.js
@@ -1,62 +1,57 @@
-import renderer from "react-test-renderer";
-import SignIn from "pages/LandingPages/SignIn";
+// React testing libraries
 import React from "react";
+import renderer from "react-test-renderer";
+
+// Component to test
+import SignIn from "pages/LandingPages/SignIn";
 
 let realUseContext;
 let useContextMock;
 // Setup mock
 beforeEach(() => {
-    realUseContext = React.useContext;
-    useContextMock = React.useContext = jest.fn();
+  realUseContext = React.useContext;
+  useContextMock = React.useContext = jest.fn();
 });
 // Cleanup mock
 afterEach(() => {
-    React.useContext = realUseContext;
+  React.useContext = realUseContext;
 });
 
 // Define Mocks
 jest.mock("examples/Navbars/DefaultNavbar", () => {
-    const { forwardRef } = jest.requireActual("react");
-    return {
-        __esModule: true,
-        default: forwardRef(() => <div>Mock Navbar</div>)
-    };
+  const { forwardRef } = jest.requireActual("react");
+  return {
+    __esModule: true,
+    default: forwardRef(() => <div>Mock Navbar</div>),
+  };
 });
 jest.mock("components/DHSnackbar", () => {
-    const { forwardRef } = jest.requireActual("react");
-    return {
-        __esModule: true,
-        default: forwardRef(() => <div>Mock Snackbar</div>)
-    };
+  const { forwardRef } = jest.requireActual("react");
+  return {
+    __esModule: true,
+    default: forwardRef(() => <div>Mock Snackbar</div>),
+  };
 });
 jest.mock("components/MKBox", () => {
-    const { forwardRef } = jest.requireActual("react");
-    return {
-        __esModule: true,
-        default: forwardRef(() => <div>Mock Box</div>)
-    };
+  const { forwardRef } = jest.requireActual("react");
+  return {
+    __esModule: true,
+    default: forwardRef(() => <div>Mock Box</div>),
+  };
 });
 jest.mock("react-monaco-editor", () => {
-    const { forwardRef } = jest.requireActual("react");
-    return {
-        __esModule: true,
-        default: forwardRef(() => <div>Mock Editor</div>)
-    };
-});
-
-jest.mock("context/AuthContext", () => {
-    const { createContext } = jest.requireActual('react'); 
-    return {
-        __esModule: true,
-        default: createContext()
-    }
+  const { forwardRef } = jest.requireActual("react");
+  return {
+    __esModule: true,
+    default: forwardRef(() => <div>Mock Editor</div>),
+  };
 });
 
 describe("SignIn", () => {
-    it("renders", () => {
-        useContextMock.mockReturnValue("Test Value");
-        const component = renderer.create(<SignIn />);
-        let tree = component.toJSON();
-        expect(tree).toMatchSnapshot();
-    });
+  it("renders", () => {
+    useContextMock.mockReturnValue("Test Value");
+    const component = renderer.create(<SignIn />);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
