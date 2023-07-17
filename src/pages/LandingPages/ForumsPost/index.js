60d74d7255d827193e252672678b90a3a7606cc4
dhong9 <danielhong35@gmail.com>
2023-07-16 17:38:28 -0700

    Parsed forums post data when loaded

--- src/pages/LandingPages/ForumsPost/index.js
+++ src/pages/LandingPages/ForumsPost/index.js
@@ -1,5 +1,5 @@
 // React
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 // Services
@@ -7,9 +7,23 @@ import { getPostById } from "services/postsService";
 
 export default function ForumsPost() {
   const { id } = useParams();
+  const [post, setPost] = useState(null);
 
   // Get post By id
-  useEffect(getPostById(id, console.log), []);
+  useEffect(
+    getPostById(id, ({ data }) => {
+      setPost(data);
+    }),
+    []
+  );
 
-  return <div>Form post with ID {id}</div>;
+  return (
+    <div>
+      <h1>{post.postName}</h1>
+      <h2>
+        {post.name} {post.create}
+      </h2>
+      <p>{post.body}</p>
+    </div>
+  );
 }
