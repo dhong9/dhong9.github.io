0b6875844a1af8f52a87532f4794a384ed794054
dhong9 <danielhong35@gmail.com>
2023-07-16 21:09:13 -0700

    Added some sample posts to MainForumPosts unit test

--- src/components/MainForumPosts/MainForumPosts.test.js
+++ src/components/MainForumPosts/MainForumPosts.test.js
@@ -1,16 +1,34 @@
 // Unit test libraries
 import renderer from "react-test-renderer";
 
+// Material Kit 2 React themes
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "assets/theme";
+
 // Component to test
 import MainForumPosts from "components/MainForumPosts";
 
 describe("MainForumPosts", () => {
   it("renders", () => {
+    const posts = [
+      {
+        id: 1,
+        isPlainText: false,
+        body: "**Hi** _there_!",
+      },
+      {
+        id: 2,
+        isPlainText: true,
+        body: "Hello again!",
+      },
+    ];
+    const title = "Test Post";
 
-    const posts = []
-    const title = "Test Post"
-
-    const component = renderer.create(<MainForumPosts posts={posts} title={title} />);
+    const component = renderer.create(
+      <ThemeProvider theme={theme}>
+        <MainForumPosts posts={posts} title={title} />
+      </ThemeProvider>
+    );
 
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
