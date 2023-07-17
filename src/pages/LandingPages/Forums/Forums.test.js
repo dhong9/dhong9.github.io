1202a78bd41ad1ff74a6252de6e6397d46b945d3
dhong9 <danielhong35@gmail.com>
2023-07-16 17:06:11 -0700

    Added unit test for Forums page

---
+++ src/pages/LandingPages/Forums/Forums.test.js
@@ -0,0 +1,45 @@
+// Unit test libraries
+import React from "react";
+import renderer from "react-test-renderer";
+
+// Component to test
+import Forums from "pages/LandingPages/Forums";
+
+let realUseContext;
+let useContextMock;
+// Setup mock
+beforeEach(() => {
+  realUseContext = React.useContext;
+  useContextMock = React.useContext = jest.fn();
+});
+// Cleanup mock
+afterEach(() => {
+  React.useContext = realUseContext;
+});
+
+// Mocks
+jest.mock("react-monaco-editor", () => {
+  const { forwardRef } = jest.requireActual("react");
+  return {
+    __esModule: true,
+    default: forwardRef(() => <div>Mock Editor</div>),
+  };
+});
+jest.mock("components/MKBox", () => {
+    const { forwardRef } = jest.requireActual("react");
+    return {
+        __esModule: true,
+        default: forwardRef(() => <div>Mock Box</div>)
+    };
+});
+
+describe("Forums", () => {
+  it("renders", () => {
+    useContextMock.mockReturnValue("Test Value");
+
+    const component = renderer.create(<Forums />);
+
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
