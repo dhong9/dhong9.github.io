d4ef36e4e0f39a2f21dedd793b8a8686a11ac25d
dhong9 <danielhong35@gmail.com>
2023-07-16 17:15:35 -0700

    Logs post with specified ID

--- src/pages/LandingPages/ForumsPost/index.js
+++ src/pages/LandingPages/ForumsPost/index.js
@@ -1,7 +1,15 @@
+// React
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+
+// Services
+import { getPostById } from "services/postsService";
 
 export default function ForumsPost() {
   const { id } = useParams();
+
+  // Get post By id
+  useEffect(getPostById(id, console.log), []);
 
   return <div>Form post with ID {id}</div>;
 }
