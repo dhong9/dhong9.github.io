536f9053a1d48eb53a9a0be972b7c1780ea09533
dhong9 <danielhong35@gmail.com>
2023-07-18 18:12:18 -0700

    Debugging with hardcoded post ID

--- src/pages/LandingPages/ForumsPost/index.js
+++ src/pages/LandingPages/ForumsPost/index.js
@@ -1,22 +1,22 @@
 // React
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+//import { useParams } from "react-router-dom";
 
 // Sections components
+import FeaturedPost from "components/FeaturedPost";
 import BaseLayout from "layouts/sections/components/BaseLayout";
 
 // Services
 import { getPostById } from "services/postsService";
 
 export default function ForumsPost() {
-  const { id } = useParams();
+  //const { id } = useParams();
   const [post, setPost] = useState(null);
 
   // Get post By id
   useEffect(
-    getPostById(id, ({ data }) => {
+    getPostById(1, ({ data }) => {
       setPost(data);
-      console.log(post);
     }),
     []
   );
@@ -25,6 +25,14 @@ export default function ForumsPost() {
     <BaseLayout
       title="Doughnut Rider"
       breadcrumb={[{ label: "Doughnut Rider", route: "/doughnutRider" }]}
-    ></BaseLayout>
+    >
+      {post ? (
+        <div>
+          <FeaturedPost post={post} />
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </BaseLayout>
   );
 }
