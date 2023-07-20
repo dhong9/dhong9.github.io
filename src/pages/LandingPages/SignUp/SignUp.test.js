e0411aa6e1275e3149a8a80dcd4b152382cf7399
dhong9 <danielhong35@gmail.com>
2023-07-19 21:00:00 -0700

    Wrapped SignUp test in ThemeProvider

--- src/pages/LandingPages/SignUp/SignUp.test.js
+++ src/pages/LandingPages/SignUp/SignUp.test.js
@@ -1,54 +1,44 @@
-import renderer from "react-test-renderer";
-import SignUp from "pages/LandingPages/SignUp";
+// React testing libraries
 import React from "react";
+import renderer from "react-test-renderer";
+
+// Material Kit 2 React themes
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "assets/theme";
+
+// Component to test
+import SignUp from "pages/LandingPages/SignUp";
 
 let realUseContext;
 let useContextMock;
 // Setup mock
 beforeEach(() => {
-    realUseContext = React.useContext;
-    useContextMock = React.useContext = jest.fn();
+  realUseContext = React.useContext;
+  useContextMock = React.useContext = jest.fn();
 });
 // Cleanup mock
 afterEach(() => {
-    React.useContext = realUseContext;
+  React.useContext = realUseContext;
 });
 
 // Define Mocks
-jest.mock("examples/Navbars/DefaultNavbar", () => {
-    const { forwardRef } = jest.requireActual("react");
-    return {
-        __esModule: true,
-        default: forwardRef(() => <div>Mock Navbar</div>)
-    };
-});
-jest.mock("components/DHSnackbar", () => {
-    const { forwardRef } = jest.requireActual("react");
-    return {
-        __esModule: true,
-        default: forwardRef(() => <div>Mock Snackbar</div>)
-    };
-});
-jest.mock("components/MKBox", () => {
-    const { forwardRef } = jest.requireActual("react");
-    return {
-        __esModule: true,
-        default: forwardRef(() => <div>Mock Box</div>)
-    };
-});
 jest.mock("react-monaco-editor", () => {
-    const { forwardRef } = jest.requireActual("react");
-    return {
-        __esModule: true,
-        default: forwardRef(() => <div>Mock Editor</div>)
-    };
+  const { forwardRef } = jest.requireActual("react");
+  return {
+    __esModule: true,
+    default: forwardRef(() => <div>Mock Editor</div>),
+  };
 });
 
 describe("SignUp", () => {
-    it("renders", () => {
-        useContextMock.mockReturnValue("Test Value");
-        const component = renderer.create(<SignUp />);
-        let tree = component.toJSON();
-        expect(tree).toMatchSnapshot();
-    });
+  it("renders", () => {
+    useContextMock.mockReturnValue("Test Value");
+    const component = renderer.create(
+      <ThemeProvider theme={theme}>
+        <SignUp />
+      </ThemeProvider>
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
