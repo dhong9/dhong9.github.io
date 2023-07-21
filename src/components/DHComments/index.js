61ec79a1bfd0d818fa8c18dc36552b46bb16c060
dhong9 <danielhong35@gmail.com>
2023-07-21 12:05:57 -0700

    Placeholder function to handle comment reply

--- src/components/DHComments/index.js
+++ src/components/DHComments/index.js
@@ -5,7 +5,6 @@ import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
-import CardActionArea from "@mui/material/CardActionArea";
 import CardContent from "@mui/material/CardContent";
 
 // prop-types is a library for typechecking of props
@@ -14,28 +13,30 @@ import PropTypes from "prop-types";
 import DHMarkdown from "components/DHMarkdown";
 
 export default function DHComments({ comments }) {
+  const handleReply = (e) => {
+    console.log(e.target.value);
+  };
+
   return (
     <Paper style={{ padding: "40px 20px" }}>
       {comments.map(({ id, body, name, create, isPlainText }) => (
         <Grid key={id} item xs={12} md={6}>
-          <CardActionArea component="a" href="#">
-            <Card sx={{ display: "flex" }}>
-              <CardContent sx={{ flex: 1 }}>
-                <Typography component="h2" variant="h5">
-                  {name}
-                </Typography>
-                <Typography variant="subtitle1" color="text.secondary">
-                  {new Date(create).toLocaleString()}
-                </Typography>
-                <Typography variant="subtitle1" paragraph>
-                  {isPlainText ? body : <DHMarkdown>{body}</DHMarkdown>}
-                </Typography>
-                <Typography variant="subtitle1" color="primary">
-                  Reply...
-                </Typography>
-              </CardContent>
-            </Card>
-          </CardActionArea>
+          <Card sx={{ display: "flex" }}>
+            <CardContent sx={{ flex: 1 }}>
+              <Typography component="h2" variant="h5">
+                {name}
+              </Typography>
+              <Typography variant="subtitle1" color="text.secondary">
+                {new Date(create).toLocaleString()}
+              </Typography>
+              <Typography variant="subtitle1" paragraph>
+                {isPlainText ? body : <DHMarkdown>{body}</DHMarkdown>}
+              </Typography>
+              <Typography variant="a" color="primary" onClick={handleReply}>
+                Reply...
+              </Typography>
+            </CardContent>
+          </Card>
         </Grid>
       ))}
     </Paper>
