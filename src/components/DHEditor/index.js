781a84e4a4d0b1ad76da741f2bd9b2276db4182a
dhong9 <danielhong35@gmail.com>
2023-07-21 22:25:58 -0700

    Added some CSS to editor

--- src/components/DHEditor/index.js
+++ src/components/DHEditor/index.js
@@ -4,10 +4,22 @@ import { useState } from "react";
 // Editor
 import { EditorState } from "draft-js";
 import { Editor } from "react-draft-wysiwyg";
-import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
+// CSS
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import "components/DHEditor/DHEditor.css";
+
+// https://blog.logrocket.com/build-rich-text-editors-react-draft-js-react-draft-wysiwyg/
 export default function DHEditor() {
   const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
 
-  return <Editor editorState={editorState} onEditorStateChange={setEditorState} />;
+  return (
+    <Editor
+      editorState={editorState}
+      onEditorStateChange={setEditorState}
+      wrapperClassName="wrapper-class"
+      editorClassName="editor-class"
+      toolbarClassName="toolbar-class"
+    />
+  );
 }
