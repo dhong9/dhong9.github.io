184217d49c6aa7a1c83e3d01200faeb3c8abfa97
dhong9 <danielhong35@gmail.com>
2023-07-21 20:47:02 -0700

    Convert example Editor component to functional

--- src/components/DHEditor/index.js
+++ src/components/DHEditor/index.js
@@ -1,29 +1,24 @@
 // React
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 // Editor
 import { Editor, EditorState, RichUtils } from "draft-js";
 import "draft-js/dist/Draft.css";
 
 export default function DHEditor() {
-  const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
 
   // Handle common key command (i.e. copy/paste)
-  const handleKeyCommand = (command, editorState) => {
+  const handleKeyCommand = useCallback((command, editorState) => {
     const newState = RichUtils.handleKeyCommand(editorState, command);
-
     if (newState) {
-      setEditorState(() => newState);
+      setEditorState(newState);
     }
-  };
+  });
 
   // Styling controls
   const _onBoldClick = () => {
-    const newState = RichUtils.toggleInlineStyle(editorState, "BOLD");
-
-    if (newState) {
-      setEditorState(() => newState);
-    }
+    setEditorState(RichUtils.toggleInlineStyle(editorState, "BOLD"));
   };
 
   return (
