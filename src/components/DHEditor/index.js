fbaaf417cfb85e0f1aa78d3287385e007bae5ef7
dhong9 <danielhong35@gmail.com>
2023-07-21 22:08:11 -0700

    Added use of React Draft WYSIWYG

--- src/components/DHEditor/index.js
+++ src/components/DHEditor/index.js
@@ -1,40 +1,13 @@
 // React
-import { useState, useCallback } from "react";
+import { useState } from "react";
 
 // Editor
-import { Editor, EditorState, RichUtils } from "draft-js";
-import "draft-js/dist/Draft.css";
+import { EditorState } from "draft-js";
+import { Editor } from "react-draft-wysiwyg";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
 export default function DHEditor() {
   const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
 
-  // Handle common key command (i.e. copy/paste)
-  const handleKeyCommand = useCallback((command, editorState) => {
-    const newState = RichUtils.handleKeyCommand(editorState, command);
-    if (newState) {
-      setEditorState(newState);
-    }
-  });
-
-  // Styling controls
-  const handleInlineStyle = (e, style) => {
-    e.preventDefault();
-    setEditorState(RichUtils.toggleInlineStyle(editorState, style));
-  };
-
-  return (
-    <>
-      <div>
-        <button onMouseDown={(e) => handleInlineStyle(e, "BOLD")}>Bold</button>
-        <button onMouseDown={(e) => handleInlineStyle(e, "ITALIC")}>Italic</button>
-        <button onMouseDown={(e) => handleInlineStyle(e, "UNDERLINE")}>Underline</button>
-        <button onMouseDown={(e) => handleInlineStyle(e, "CODE")}>Code</button>
-      </div>
-      <Editor
-        editorState={editorState}
-        onChange={setEditorState}
-        handleKeyCommand={handleKeyCommand}
-      />
-    </>
-  );
+  return <Editor editorState={editorState} onEditorStateChange={setEditorState} />;
 }
