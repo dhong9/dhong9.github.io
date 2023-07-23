bcc5b00a300305a8cfdd9299db6368018de807ab
dhong9 <danielhong35@gmail.com>
2023-07-22 19:34:21 -0700

    Updated DHEditor unit test to test for event changes

--- src/components/DHEditor/DHEditor.test.js
+++ src/components/DHEditor/DHEditor.test.js
@@ -1,5 +1,6 @@
 // React testing libraries
-import renderer from "react-test-renderer";
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 
 // Component to test
 import DHEditor from "components/DHEditor";
@@ -9,13 +10,23 @@ jest.mock("draft-convert", () => {
   return {
     convertFromHTML: jest.fn(),
     convertToRaw: jest.fn(),
+    convertToHTML: jest.fn().mockReturnValue("<p>Test content</p>"), // Mock the return value of convertToHTML
   };
 });
 
 describe("DHEditor", () => {
-  it("renders", () => {
-    const component = renderer.create(<DHEditor />);
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+  it("updates the convertedContent", () => {
+    const { getByText, getByLabelText  } = render(<DHEditor />);
+
+    // Initially, the convertedContent should not be rendered in the document
+    expect(getByText("Test content")).toBeInTheDocument();
+
+    // Find the contenteditable text box using the aria-label attribute
+    const textBox = getByLabelText("rdw-editor");;
+
+    // Trigger a change in the editorState by typing into the text box
+    fireEvent.input(textBox, { target: { textContent: "New content" } });
+    // After the change, the convertedContent should be rendered with the mocked HTML content
+    expect(getByText("New content")).toBeInTheDocument();
   });
 });
