b9c5295ef793e0d15440b6e1b744157bcbe56744
dhong9 <danielhong35@gmail.com>
2023-07-24 19:53:00 -0700

    Added unit test for BrainF project

---
+++ src/projects/interpreters/brainF/BrainF.test.js
@@ -0,0 +1,74 @@
+// React testing libraries
+import React from "react";
+import renderer from "react-test-renderer";
+
+// @mui material components
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "assets/theme";
+
+// Component to test
+import BrainF from "projects/interpreters/brainF";
+
+let realUseContext;
+let useContextMock;
+// Setup mock
+beforeEach(() => {
+  realUseContext = React.useContext;
+  useContextMock = React.useContext = jest.fn();
+});
+// Cleanup mock
+afterEach(() => {
+  React.useContext = realUseContext;
+});
+
+// Mocks
+jest.mock("components/MKButton", () => {
+  const { forwardRef } = jest.requireActual("react");
+  return {
+    __esModule: true,
+    default: forwardRef(() => <button>Mock Button</button>),
+  };
+});
+jest.mock("components/MKBox", () => {
+  const { forwardRef } = jest.requireActual("react");
+  return {
+    __esModule: true,
+    default: forwardRef(() => <div>Mock Box</div>),
+  };
+});
+jest.mock("layouts/sections/components/BaseLayout", () => {
+  const { forwardRef } = jest.requireActual("react");
+  return {
+    __esModule: true,
+    default: forwardRef(() => <div>Mock Layout</div>),
+  };
+});
+jest.mock("layouts/sections/components/View", () => {
+  const { forwardRef } = jest.requireActual("react");
+  return {
+    __esModule: true,
+    default: forwardRef(() => <div>Mock View</div>),
+  };
+});
+jest.mock("react-monaco-editor", () => {
+  const { forwardRef } = jest.requireActual("react");
+  return {
+    __esModule: true,
+    default: forwardRef(() => <div>Mock Editor</div>),
+  };
+});
+jest.mock("draft-convert", () => {
+  return {
+    convertFromHTML: jest.fn(),
+    convertToRaw: jest.fn(),
+  };
+});
+
+describe("BrainF", () => {
+  it("renders", () => {
+    useContextMock.mockReturnValue("Test Value");
+    const component = renderer.create(<BrainF />);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
