ff3b3d8abd6d49f86a3e0cbfd5025bf004912b42
dhong9 <danielhong35@gmail.com>
2023-07-27 20:26:16 -0700

    Created test contextData

--- src/context/AuthContext.test.js
+++ src/context/AuthContext.test.js
@@ -1,20 +1,41 @@
 // React testing libraries
 import renderer from "react-test-renderer";
+import { useContext } from "react";
+
+// Service functions
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
+import { getRequest, postRequest } from "services/baseService";
+const mock = new MockAdapter(axios);
 
 // Component to test
-import { AuthProvider } from "context/AuthContext";
+import AuthContext, { AuthProvider } from "context/AuthContext";
 
 // Mocks
 jest.mock("react-router-dom", () => ({
-    useNavigate: jest.fn,
-  }));
+  useNavigate: jest.fn,
+}));
+
+jest.mock("services/baseService", () => ({
+  getRequest: jest.fn(),
+  postRequest: jest.fn(),
+}));
+
+mock.onGet("/accounts/token/").reply(200, {});
+mock.onPost("/accounts/token/").reply(200, {});
 
 describe("AuthContext", () => {
   it("renders", () => {
+    const contextData = {
+      loginUser: jest.fn()
+    };
+
     const component = renderer.create(
-      <AuthProvider>
-        <div>Hello world!</div>
-      </AuthProvider>
+      <AuthContext.Provider value={contextData}>
+        <AuthProvider>
+          <div>Hello world!</div>
+        </AuthProvider>
+      </AuthContext.Provider>
     );
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
