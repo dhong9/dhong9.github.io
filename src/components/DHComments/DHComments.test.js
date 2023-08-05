b92473a53841874f2cf8686c5d551330c1d80304
dhong9 <danielhong35@gmail.com>
2023-08-05 15:24:39 -0700

    Used ThemeProvider on DHComments test

--- src/components/DHComments/DHComments.test.js
+++ src/components/DHComments/DHComments.test.js
@@ -1,52 +1,49 @@
+// React testing libraries
 import React from "react";
 import renderer from "react-test-renderer";
+
+// Material Kit 2 React themes
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "assets/theme";
+
+// Component to test
 import DHComments from "components/DHComments";
 
 // Mocks
-jest.mock("components/MKButton", () => {
-    const { forwardRef } = jest.requireActual("react");
-    return {
-        __esModule: true,
-        default: forwardRef(() => <button>Mock Button</button>)
-    };
-});
-jest.mock("components/MKTypography", () => {
-    const { forwardRef } = jest.requireActual("react");
-    return {
-        __esModule: true,
-        default: forwardRef(() => <p>Mock Typography</p>)
-    };
-});
 
 describe("DHComments", () => {
-    let useEffect;
+  let useEffect;
 
-    const mockUseEffect = () => {
-        useEffect.mockImplementationOnce(f => f());
-    };
+  const mockUseEffect = () => {
+    useEffect.mockImplementationOnce((f) => f());
+  };
 
-    beforeEach(() => {
-        useEffect = jest.spyOn(React, "useEffect");
-        mockUseEffect();
-    });
+  beforeEach(() => {
+    useEffect = jest.spyOn(React, "useEffect");
+    mockUseEffect();
+  });
 
-    it("renders", () => {
-        const comments = [{
-            "id": 1,
-            "pageName": "connect4",
-            "name": "John Adams",
-            "email": "john_adams@aol.com",
-            "body": "Play connect 4 with me",
-            "create": "2023-05-26T17:42:43.263337Z",
-            "updated": "2023-05-26T17:42:43.263383Z",
-            "active": true,
-            "parent": null
-        }];
-        
-        const component = renderer.create(<DHComments comments={comments} />);
-        let tree = component.toJSON();
-        expect(tree).toMatchSnapshot();
-    });
+  it("renders", () => {
+    const comments = [
+      {
+        id: 1,
+        project: 2,
+        name: "John Adams",
+        email: "john_adams@aol.com",
+        body: "Play connect 4 with me",
+        create: "2023-05-26T17:42:43.263337Z",
+        updated: "2023-05-26T17:42:43.263383Z",
+        active: true,
+        parent: null,
+      },
+    ];
 
-
+    const component = renderer.create(
+      <ThemeProvider theme={theme}>
+        <DHComments comments={comments} />
+      </ThemeProvider>
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
