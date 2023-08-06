156e5eb7f92ee53056681b18d1b4c3cd84395bed
dhong9 <danielhong35@gmail.com>
2023-08-05 21:20:35 -0700

    Added draft-convert mock to Othello unit test

--- src/projects/games/othello/Othello.test.js
+++ src/projects/games/othello/Othello.test.js
@@ -9,57 +9,63 @@ let realUseContext;
 let useContextMock;
 // Setup mock
 beforeEach(() => {
-    realUseContext = React.useContext;
-    useContextMock = React.useContext = jest.fn();
+  realUseContext = React.useContext;
+  useContextMock = React.useContext = jest.fn();
 });
 // Cleanup mock
 afterEach(() => {
-    React.useContext = realUseContext;
+  React.useContext = realUseContext;
 });
-
 
 // Define Mocks
 jest.mock("components/MKButton", () => {
-    const { forwardRef } = jest.requireActual("react");
-    return {
-        __esModule: true,
-        default: forwardRef(() => <button>Mock Button</button>)
-    };
+  const { forwardRef } = jest.requireActual("react");
+  return {
+    __esModule: true,
+    default: forwardRef(() => <button>Mock Button</button>),
+  };
 });
 jest.mock("components/MKBox", () => {
-    const { forwardRef } = jest.requireActual("react");
-    return {
-        __esModule: true,
-        default: forwardRef(() => <div>Mock Box</div>)
-    };
+  const { forwardRef } = jest.requireActual("react");
+  return {
+    __esModule: true,
+    default: forwardRef(() => <div>Mock Box</div>),
+  };
 });
 jest.mock("layouts/sections/components/BaseLayout", () => {
-    const { forwardRef } = jest.requireActual("react");
-    return {
-        __esModule: true,
-        default: forwardRef(() => <div>Mock Layout</div>)
-    };
+  const { forwardRef } = jest.requireActual("react");
+  return {
+    __esModule: true,
+    default: forwardRef(() => <div>Mock Layout</div>),
+  };
 });
 jest.mock("layouts/sections/components/View", () => {
-    const { forwardRef } = jest.requireActual("react");
-    return {
-        __esModule: true,
-        default: forwardRef(() => <div>Mock View</div>)
-    };
+  const { forwardRef } = jest.requireActual("react");
+  return {
+    __esModule: true,
+    default: forwardRef(() => <div>Mock View</div>),
+  };
 });
 jest.mock("react-monaco-editor", () => {
-    const { forwardRef } = jest.requireActual("react");
-    return {
-        __esModule: true,
-        default: forwardRef(() => <div>Mock Editor</div>)
-    };
+  const { forwardRef } = jest.requireActual("react");
+  return {
+    __esModule: true,
+    default: forwardRef(() => <div>Mock Editor</div>),
+  };
+});
+jest.mock("draft-convert", () => {
+  return {
+    convertFromHTML: jest.fn(),
+    convertToRaw: jest.fn(),
+    convertToHTML: jest.fn().mockReturnValue("<p>Test content</p>"), // Mock the return value of convertToHTML
+  };
 });
 
 describe("Othello", () => {
-    it("renders", () => {
-        useContextMock.mockReturnValue("Test Value");
-        const component = renderer.create(<Othello />);
-        let tree = component.toJSON();
-        expect(tree).toMatchSnapshot();
-    });
+  it("renders", () => {
+    useContextMock.mockReturnValue("Test Value");
+    const component = renderer.create(<Othello />);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
