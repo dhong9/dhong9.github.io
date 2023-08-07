1be633e7fa6a3068c5108870db6ac00b28111495
dhong9 <danielhong35@gmail.com>
2023-08-06 21:11:18 -0700

    Restored unit test for token update

--- src/context/AuthContext.test.js
+++ src/context/AuthContext.test.js
@@ -1,6 +1,9 @@
 // React testing libraries
 import renderer from "react-test-renderer";
 
+// JWT
+import jwtDecode from "jwt-decode";
+
 // Component to test
 import AuthContext, { AuthProvider } from "context/AuthContext";
 
@@ -14,7 +17,27 @@ jest.mock("services/baseService", () => ({
   postRequest: jest.fn(),
 }));
 
+jest.mock("jwt-decode");
+
+// Mock JWT components
+const mockToken = "mocked_jwt_value";
+const refreshToken = "mocked_refresh_value";
+
+// Helper function to set mock tokens in localStorage
+const setMockTokensInLocalStorage = () => {
+  localStorage.setItem("authTokens", JSON.stringify({ access: mockToken, refresh: refreshToken }));
+};
+
 describe("AuthContext", () => {
+  beforeEach(() => {
+    localStorage.clear();
+  });
+
+  afterEach(() => {
+    localStorage.removeItem("authTokens");
+    jest.clearAllMocks();
+  });
+
   it("renders", () => {
     const contextData = {
       loginUser: jest.fn(),
@@ -27,5 +50,33 @@ describe("AuthContext", () => {
     );
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it("updates token", () => {
+    localStorage.setItem(
+      "authTokens",
+      JSON.stringify({ access: mockToken, refresh: refreshToken })
+    );
+
+    // Set a mock payload for the decoded token
+    const mockPayload = { user: "John Doe", exp: 1893456000 };
+    jwtDecode.mockReturnValue(mockPayload);
+
+    const contextData = {
+      loginUser: jest.fn(),
+    };
+
+    const component = renderer.create(
+      <AuthContext.Provider value={contextData}>
+        <AuthProvider>Hello world!</AuthProvider>
+      </AuthContext.Provider>
+    );
+
+    jest.useFakeTimers();
+    setTimeout(() => {
+      let tree = component.toJSON();
+      expect(tree).toMatchSnapshot();
+    }, 4 * 1000 * 60);
+    jest.runAllTimers();
   });
 });
