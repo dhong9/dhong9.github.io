54b6299c826e77b6f925100b6232bfc415df531a
dhong9 <danielhong35@gmail.com>
2023-08-06 18:16:07 -0700

    Removed useEffect from AuthContext

--- src/context/AuthContext.test.js
+++ src/context/AuthContext.test.js
@@ -1,10 +1,11 @@
 // React testing libraries
 import renderer from "react-test-renderer";
-import React from "react";
+import { useContext } from "react";
 
 // Service functions
 import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
+import { getRequest, postRequest } from "services/baseService";
 const mock = new MockAdapter(axios);
 
 // Component to test
@@ -24,17 +25,6 @@ mock.onGet("/accounts/token/").reply(200, {});
 mock.onPost("/accounts/token/").reply(200, {});
 
 describe("AuthContext", () => {
-  let useEffect;
-
-  const mockUseEffect = () => {
-    useEffect.mockImplementationOnce((f) => f());
-  };
-
-  beforeEach(() => {
-    useEffect = jest.spyOn(React, "useEffect");
-    mockUseEffect();
-  });
-
   it("renders", () => {
     const contextData = {
       loginUser: jest.fn()
