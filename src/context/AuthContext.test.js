8b4e694ae66f5c5c5188010f479ea2983b39d931
dhong9 <danielhong35@gmail.com>
2023-08-06 21:01:20 -0700

    Forgoing idea of testing mock token

--- src/context/AuthContext.test.js
+++ src/context/AuthContext.test.js
@@ -1,13 +1,6 @@
 // React testing libraries
 import renderer from "react-test-renderer";
 
-// JWT
-import jwtDecode from "jwt-decode";
-
-// Service functions
-import axios from "axios";
-import MockAdapter from "axios-mock-adapter";
-
 // Component to test
 import AuthContext, { AuthProvider } from "context/AuthContext";
 
@@ -21,30 +14,7 @@ jest.mock("services/baseService", () => ({
   postRequest: jest.fn(),
 }));
 
-const mock = new MockAdapter(axios);
-mock.onGet("/accounts/token/refresh/").reply(200, { access: "new_token", refresh: "new_refresh" });
-
-jest.mock("jwt-decode");
-
-// Mock JWT components
-const mockToken = "mocked_jwt_value";
-const refreshToken = "mocked_refresh_value";
-
-// Helper function to set mock tokens in localStorage
-const setMockTokensInLocalStorage = () => {
-  localStorage.setItem("authTokens", JSON.stringify({ access: mockToken, refresh: refreshToken }));
-};
-
 describe("AuthContext", () => {
-  beforeEach(() => {
-    localStorage.clear();
-  });
-
-  afterEach(() => {
-    localStorage.removeItem("authTokens");
-    jest.clearAllMocks();
-  });
-
   it("renders", () => {
     const contextData = {
       loginUser: jest.fn(),
@@ -57,30 +27,5 @@ describe("AuthContext", () => {
     );
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
-  });
-
-  it("updates token - valid refresh token", () => {
-    // Set the mock token in the localStorage before rendering the component
-    setMockTokensInLocalStorage();
-
-    const mockPayload = { user: "John Doe", exp: 1893456000 };
-    jwtDecode.mockReturnValueOnce(mockPayload);
-
-    const contextData = {
-      loginUser: jest.fn(),
-    };
-
-    const component = renderer.create(
-      <AuthContext.Provider value={contextData}>
-        <AuthProvider>Hello world!</AuthProvider>
-      </AuthContext.Provider>
-    );
-
-    jest.useFakeTimers();
-    setTimeout(() => {
-      let tree = component.toJSON();
-      expect(tree).toMatchSnapshot();
-    }, 4 * 1000 * 60);
-    jest.runAllTimers();
   });
 });
