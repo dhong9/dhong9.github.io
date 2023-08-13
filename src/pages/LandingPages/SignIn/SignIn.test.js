231e177ce8788b0d6317644725f75509bb8b8df4
dhong9 <danielhong35@gmail.com>
2023-08-13 15:43:04 -0700

    Renders entire SignIn at unit test

--- src/pages/LandingPages/SignIn/SignIn.test.js
+++ src/pages/LandingPages/SignIn/SignIn.test.js
@@ -1,41 +1,32 @@
 // React testing libraries
-import React from "react";
-import renderer from "react-test-renderer";
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
 
-// @mui material components
+// Material Kit 2 React themes
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "assets/theme";
+
+// Authentication
+import axios from "axios";
+import AuthContext, { AuthProvider } from "context/AuthContext";
+
+// Axios
+import MockAdapter from "axios-mock-adapter";
 
 // Component to test
 import SignIn from "pages/LandingPages/SignIn";
 
-let realUseContext;
-let useContextMock;
-// Setup mock
-beforeEach(() => {
-  realUseContext = React.useContext;
-  useContextMock = React.useContext = jest.fn();
-});
-// Cleanup mock
-afterEach(() => {
-  React.useContext = realUseContext;
-});
+jest.mock("services/baseService", () => ({
+  getRequest: jest.fn(),
+  postRequest: jest.fn(),
+}));
+
+// Setup axios mock
+const mock = new MockAdapter(axios);
+
+mock.onPost("accounts/token/").reply(200, {});
 
 // Define Mocks
-jest.mock("examples/Navbars/DefaultNavbar", () => {
-  const { forwardRef } = jest.requireActual("react");
-  return {
-    __esModule: true,
-    default: forwardRef(() => <div>Mock Navbar</div>),
-  };
-});
-jest.mock("components/MKBox", () => {
-  const { forwardRef } = jest.requireActual("react");
-  return {
-    __esModule: true,
-    default: forwardRef(() => <div>Mock Box</div>),
-  };
-});
 jest.mock("react-monaco-editor", () => {
   const { forwardRef } = jest.requireActual("react");
   return {
@@ -46,19 +37,30 @@ jest.mock("react-monaco-editor", () => {
 jest.mock("draft-convert", () => {
   return {
     convertFromHTML: jest.fn(),
+    convertToHTML: jest.fn(),
     convertToRaw: jest.fn(),
   };
 });
+jest.mock("react-router-dom", () => ({
+  Link: jest.fn(({ to, children }) => <a href={to}>{children}</a>),
+  useNavigate: jest.fn,
+}));
 
 describe("SignIn", () => {
   it("renders", () => {
-    useContextMock.mockReturnValue("Test Value");
-    const component = renderer.create(
-      <ThemeProvider theme={theme}>
-        <SignIn />
-      </ThemeProvider>
+    const contextData = {
+      loginUser: jest.fn(),
+    };
+    const { container } = render(
+      <AuthContext.Provider value={contextData}>
+        <AuthProvider>
+          <ThemeProvider theme={theme}>
+            <SignIn />
+          </ThemeProvider>
+        </AuthProvider>
+      </AuthContext.Provider>
     );
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+    
+    expect(container).toMatchSnapshot();
   });
 });
