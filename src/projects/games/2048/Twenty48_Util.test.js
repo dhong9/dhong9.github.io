e2d988af60f96bcf5d27aa0261ac505ada7b2aa1
dhong9 <danielhong35@gmail.com>
2023-09-04 17:34:52 -0700

    Added unit tests for all moving 2048 tiles in all 4 directions

--- src/projects/games/2048/Twenty48_Util.test.js
+++ src/projects/games/2048/Twenty48_Util.test.js
@@ -5,7 +5,7 @@ describe("Twenty48_Util", () => {
     // Initialize 2048 game board
     const game = new Twenty48_Util();
 
-    it("moves tiles", () => {
+    it("moves tiles right", () => {
         const testBoard = [
             [0, 0, 0, 0],
             [0, 0, 0, 0],
@@ -19,6 +19,63 @@ describe("Twenty48_Util", () => {
             [0, 0, 0, 0],
             [0, 0, 0, 0],
             [0, 0, 0, 4]
+        ];
+
+        expect(updatedBoard).toEqual(correctBoard);
+    });
+
+    it("moves tiles left", () => {
+        const testBoard = [
+            [0, 0, 0, 0],
+            [2, 0, 0, 0],
+            [0, 4, 4, 0],
+            [0, 0, 0, 4]
+        ];
+
+        const updatedBoard = game.makeMove(testBoard, 'L');
+        const correctBoard = [
+            [0, 0, 0, 0],
+            [2, 0, 0, 0],
+            [8, 0, 0, 0],
+            [4, 0, 0, 0]
+        ];
+
+        expect(updatedBoard).toEqual(correctBoard);
+    });
+
+    it("moves tiles up", () => {
+        const testBoard = [
+            [2, 0, 0, 0],
+            [2, 0, 0, 0],
+            [8, 4, 4, 0],
+            [0, 0, 0, 4]
+        ];
+
+        const updatedBoard = game.makeMove(testBoard, 'U');
+        const correctBoard = [
+            [4, 4, 4, 4],
+            [8, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ];
+
+        expect(updatedBoard).toEqual(correctBoard);
+    });
+
+    it("moves tiles down", () => {
+        const testBoard = [
+            [2, 0, 0, 0],
+            [2, 0, 0, 0],
+            [8, 4, 4, 0],
+            [0, 0, 0, 4]
+        ];
+
+        const updatedBoard = game.makeMove(testBoard, 'D');
+        const correctBoard = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [4, 0, 0, 0],
+            [8, 4, 4, 4]
         ];
 
         expect(updatedBoard).toEqual(correctBoard);
