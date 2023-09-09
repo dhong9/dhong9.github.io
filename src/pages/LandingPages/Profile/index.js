361c4c082733832fb8c0c033ec7a37e0a7926287
dhong9 <danielhong35@gmail.com>
2023-09-09 16:55:52 -0700

    Profile page assumes user is logged in

--- src/pages/LandingPages/Profile/index.js
+++ src/pages/LandingPages/Profile/index.js
@@ -1,5 +1,4 @@
-import { useContext, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useContext, useState } from "react";
 
 // @mui material components
 import Card from "@mui/material/Card";
@@ -30,14 +29,6 @@ import bgImage from "assets/images/SF_Hologram.png";
 function Profile() {
   let { user } = useContext(AuthContext);
 
-  useEffect(() => {
-    // If the user is not logged in, redirect to sign in page
-    if (!user) {
-      const history = useNavigate();
-      history("/");
-    }
-  }, []);
-
   const [username, setUsername] = useState(user.username);
   const [email, setEmail] = useState(user.email);
   const [password, setPassword] = useState("");
@@ -64,97 +55,94 @@ function Profile() {
     ],
   };
 
-  return user ? (
-    <>
-      <DefaultNavbar routes={[...routes, accountObj]} transparent light />
-      <MKBox
-        position="absolute"
-        top={0}
-        left={0}
-        zIndex={1}
-        width="100%"
-        minHeight="100vh"
-        sx={{
-          backgroundImage: ({ functions: { linearGradient, rgba }, palette: { gradients } }) =>
-            `${linearGradient(
-              rgba(gradients.dark.main, 0.6),
-              rgba(gradients.dark.state, 0.6)
-            )}, url(${bgImage})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-        }}
-      />
-      <MKBox px={1} width="100%" height="100vh" mx="auto" position="relative" zIndex={2}>
-        <Grid container spacing={1} justifyContent="center" alignItems="center" height="100%">
-          <Grid item xs={11} sm={9} md={5} lg={4} xl={3}>
-            <Card>
-              <MKBox
-                variant="gradient"
-                bgColor="info"
-                borderRadius="lg"
-                coloredShadow="info"
-                mx={2}
-                mt={-3}
-                p={2}
-                mb={1}
-                textAlign="center"
-              >
-                <MKTypography variant="h4" fontWeight="medium" color="white" mt={1}>
-                  My Profile
-                </MKTypography>
-              </MKBox>
-              <MKBox pt={4} pb={3} px={3}>
-                <MKBox component="form" role="form">
-                  <MKBox mb={2}>
-                    <MKInput
-                      type="text"
-                      label="Username"
-                      onChange={(e) => setUsername(e.target.value)}
-                      value={username}
-                      fullWidth
-                    />
-                  </MKBox>
-                  <MKBox mb={2}>
-                    <MKInput
-                      type="email"
-                      label="Email"
-                      onChange={(e) => setEmail(e.target.value)}
-                      value={email}
-                      fullWidth
-                    />
-                  </MKBox>
-                  <MKBox mb={2}>
-                    <MKInput
-                      type="password"
-                      label="Password"
-                      onChange={(e) => setPassword(e.target.value)}
-                      fullWidth
-                    />
-                  </MKBox>
-                  <MKBox mb={2}>
-                    <MKInput
-                      type="password"
-                      label="Confirm Password"
-                      onChange={(e) => setPassword2(e.target.value)}
-                      fullWidth
-                    />
-                  </MKBox>
-                  <MKBox mt={4} mb={1}>
-                    <MKButton variant="gradient" color="info" onClick={handleSubmit} fullWidth>
-                      save changes
-                    </MKButton>
-                  </MKBox>
+  return;
+  <>
+    <DefaultNavbar routes={[...routes, accountObj]} transparent light />
+    <MKBox
+      position="absolute"
+      top={0}
+      left={0}
+      zIndex={1}
+      width="100%"
+      minHeight="100vh"
+      sx={{
+        backgroundImage: ({ functions: { linearGradient, rgba }, palette: { gradients } }) =>
+          `${linearGradient(
+            rgba(gradients.dark.main, 0.6),
+            rgba(gradients.dark.state, 0.6)
+          )}, url(${bgImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    />
+    <MKBox px={1} width="100%" height="100vh" mx="auto" position="relative" zIndex={2}>
+      <Grid container spacing={1} justifyContent="center" alignItems="center" height="100%">
+        <Grid item xs={11} sm={9} md={5} lg={4} xl={3}>
+          <Card>
+            <MKBox
+              variant="gradient"
+              bgColor="info"
+              borderRadius="lg"
+              coloredShadow="info"
+              mx={2}
+              mt={-3}
+              p={2}
+              mb={1}
+              textAlign="center"
+            >
+              <MKTypography variant="h4" fontWeight="medium" color="white" mt={1}>
+                My Profile
+              </MKTypography>
+            </MKBox>
+            <MKBox pt={4} pb={3} px={3}>
+              <MKBox component="form" role="form">
+                <MKBox mb={2}>
+                  <MKInput
+                    type="text"
+                    label="Username"
+                    onChange={(e) => setUsername(e.target.value)}
+                    value={username}
+                    fullWidth
+                  />
+                </MKBox>
+                <MKBox mb={2}>
+                  <MKInput
+                    type="email"
+                    label="Email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                    fullWidth
+                  />
+                </MKBox>
+                <MKBox mb={2}>
+                  <MKInput
+                    type="password"
+                    label="Password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    fullWidth
+                  />
+                </MKBox>
+                <MKBox mb={2}>
+                  <MKInput
+                    type="password"
+                    label="Confirm Password"
+                    onChange={(e) => setPassword2(e.target.value)}
+                    fullWidth
+                  />
+                </MKBox>
+                <MKBox mt={4} mb={1}>
+                  <MKButton variant="gradient" color="info" onClick={handleSubmit} fullWidth>
+                    save changes
+                  </MKButton>
                 </MKBox>
               </MKBox>
-            </Card>
-          </Grid>
+            </MKBox>
+          </Card>
         </Grid>
-      </MKBox>
-    </>
-  ) : (
-    <p>You must be logged in to view/edit your profile.</p>
-  );
+      </Grid>
+    </MKBox>
+  </>;
 }
 
 export default Profile;
