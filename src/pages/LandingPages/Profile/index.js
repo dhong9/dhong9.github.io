f6cd995302a2eae5efe36c741a8fcc0ff6be74a9
dhong9 <danielhong35@gmail.com>
2023-09-09 15:41:37 -0700

    Added SF hologram to profile page

--- src/pages/LandingPages/Profile/index.js
+++ src/pages/LandingPages/Profile/index.js
@@ -23,6 +23,9 @@ import MKTypography from "components/MKTypography";
 import SignOut from "layouts/pages/authentication/sign-out";
 import AuthContext from "context/AuthContext";
 
+// Images
+import bgImage from "assets/images/SF_Hologram.png";
+
 function Profile() {
   let { user } = useContext(AuthContext);
 
@@ -55,6 +58,24 @@ function Profile() {
   return (
     <>
       <DefaultNavbar routes={[...routes, accountObj]} transparent light />
+      <MKBox
+        position="absolute"
+        top={0}
+        left={0}
+        zIndex={1}
+        width="100%"
+        minHeight="100vh"
+        sx={{
+          backgroundImage: ({ functions: { linearGradient, rgba }, palette: { gradients } }) =>
+            `${linearGradient(
+              rgba(gradients.dark.main, 0.6),
+              rgba(gradients.dark.state, 0.6)
+            )}, url(${bgImage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      />
       <MKBox px={1} width="100%" height="100vh" mx="auto" position="relative" zIndex={2}>
         <Grid container spacing={1} justifyContent="center" alignItems="center" height="100%">
           <Grid item xs={11} sm={9} md={5} lg={4} xl={3}>
