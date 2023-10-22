e57ff9413fae821c7d3c83b2ebdf03b42ce50261
dhong9 <danielhong35@gmail.com>
2023-10-22 12:02:01 -0700

    Added VerifyEmail component

---
+++ src/layouts/pages/authentication/verify-email/index.js
@@ -0,0 +1,20 @@
+/**
+=========================================================
+* Danyo-1.1
+=========================================================
+
+* Copyright 2023 Danyo (https://www.danyo.tech)
+
+Coded by www.danyo.tech
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// Pages
+import VerifyEmail from "pages/LandingPages/VerifyEmail";
+
+export default function VerifyEmailPage() {
+  return <VerifyEmail />;
+}
---
+++ src/pages/LandingPages/VerifyEmail/index.js
@@ -0,0 +1,23 @@
+/**
+=========================================================
+* Danyo-1.1
+=========================================================
+
+* Copyright 2023 Danyo (https://www.danyo.tech)
+
+Coded by www.danyo.tech
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+function VerifyEmail() {
+    return (
+        <>
+            <h1>Verify Email</h1>
+        </>
+    );
+}
+
+export default VerifyEmail;
