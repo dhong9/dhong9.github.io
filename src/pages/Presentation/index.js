d718f3ae6f6dfb9e76b148a414145ffb41625197
dhong9 <danielhong35@gmail.com>
2024-01-12 17:34:07 -0800

    Sign in options case sensitivity

--- src/pages/Presentation/index.js
+++ src/pages/Presentation/index.js
@@ -125,24 +125,24 @@ function Presentation() {
 
   const signedOutOptions = [
     {
-      name: "sign in",
+      name: "Sign In",
       route: "/pages/authentication/sign-in",
       component: <SignIn />,
     },
     {
-      name: "sign up",
+      name: "Sign Up",
       route: "/pages/authentication/sign-up",
       component: <SignUp />,
     },
   ];
   const signedInOptions = [
     {
-      name: "profile",
+      name: "Profile",
       route: "/pages/authentication/profile",
       component: <Profile />,
     },
     {
-      name: "sign out",
+      name: "Sign Out",
       route: "/pages/authentication/sign-out",
       component: <SignOut />,
     },
