1109fea8f7fcb7718a47875e5cf84766c8a23db3
dhong9 <danielhong35@gmail.com>
2024-01-14 16:33:27 -0800

    Added mock navbar to Profile test

--- src/pages/LandingPages/Profile/Profile.test.js
+++ src/pages/LandingPages/Profile/Profile.test.js
@@ -47,6 +47,13 @@ jest.mock("react-router-dom", () => ({
   Link: jest.fn(({ to, children }) => <a href={to}>{children}</a>),
   useNavigate: jest.fn,
 }));
+jest.mock("examples/Navbars/DefaultNavbar", () => {
+  const { forwardRef } = jest.requireActual("react");
+  return {
+    __esModule: true,
+    default: forwardRef(() => <div>Mock Navbar</div>),
+  };
+});
 
 describe("Profile", () => {
   beforeEach(() => {
