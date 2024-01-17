9b25295a294ea71e342efbe7f4db3333361e9a0c
dhong9 <danielhong35@gmail.com>
2024-01-16 20:44:27 -0800

    Added leather text image to VerifyEmail component

--- src/pages/LandingPages/VerifyEmail/index.js
+++ src/pages/LandingPages/VerifyEmail/index.js
@@ -12,10 +12,31 @@ Coded by www.danyo.tech
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 
+// @mui material components
+import Grid from "@mui/material/Grid";
+
+// Material Kit 2 React components
+import MKBox from "components/MKBox";
+
+// Image
+import bgImage from "assets/images/illustrations/leatherText.png";
+
 function VerifyEmail() {
   return (
     <>
-      <h1>Verify Email</h1>
+      <Grid container spacing={3} alignItems="center">
+        <Grid item xs={12} lg={6}>
+          <MKBox
+            display={{ xs: "none", lg: "flex" }}
+            width="calc(100% - 2rem)"
+            height="calc(100vh - 2rem)"
+            borderRadius="lg"
+            ml={2}
+            mt={2}
+            sx={{ backgroundImage: `url(${bgImage})` }}
+          />
+        </Grid>
+      </Grid>
     </>
   );
 }
