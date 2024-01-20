43cc3df5402bf406a31856eeb82680a3c073e02c
dhong9 <danielhong35@gmail.com>
2024-01-20 14:44:04 -0800

    useState undefined

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ Coded by www.creative-tim.com
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 // react-router components
 import { Routes, Route, useLocation } from "react-router-dom";
