4919047760f53bcd71d536b80cbee1c1a2e44a7c
dhong9 <danielhong35@gmail.com>
2024-03-30 15:57:47 -0700

    Created sign up css

---
+++ src/pages/LandingPages/SignUp/SignUp.css
@@ -0,0 +1,13 @@
+/**
+=========================================================
+* Danyo-1.2
+=========================================================
+
+* Copyright 2024 Danyo (https://www.danyo.tech)
+
+Coded by www.danyo.tech
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
