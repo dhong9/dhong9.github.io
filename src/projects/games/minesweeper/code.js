289bb00a145a8991c65b3e8d9880bf6c4ec9d37a
dhong9 <danielhong35@gmail.com>
2024-04-13 18:00:01 -0700

    Added file header comments to Minesweeper sample code

--- src/projects/games/minesweeper/code.js
+++ src/projects/games/minesweeper/code.js
@@ -12,5 +12,13 @@ Coded by www.danyo.tech
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 
-const minesweeperCode = ``;
+const minesweeperCode = `/**
+* Project: Minesweeper
+* Author: Daniel Hong
+* Description: Discover all 10 mines on an 8x8 grid.
+*/
+
+/**
+ * Minesweeper tile
+ */`;
 export default minesweeperCode;
