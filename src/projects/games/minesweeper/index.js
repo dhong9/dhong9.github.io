73476b3fc87e0dcc74f39502fc5c4e717638e0c9
dhong9 <danielhong35@gmail.com>
2024-04-14 08:33:57 -0700

    Drew images on Minesweeper board

--- src/projects/games/minesweeper/index.js
+++ src/projects/games/minesweeper/index.js
@@ -45,6 +45,9 @@ function Minesweeper() {
 
   const draw = (p5) => {
     p5.background(0);
+    p5.image(flagImg, 0, 0, 100, 100);
+    p5.image(mineImg, 100, 0, 100, 100);
+    p5.image(smileImg, 0, 100, 100, 100);
   };
 
   return (
