73fd0c525e6eaab200a0613dab1a8b495605b5bc
dhong9 <danielhong35@gmail.com>
2024-04-15 20:35:56 -0700

    Added unit test for Minesweeper

---
+++ src/projects/games/minesweeper/Minesweeper.test.js
@@ -0,0 +1,162 @@
+// React testing libraries
+import { render, fireEvent } from "@testing-library/react";
+
+// Material Kit 2 React themes
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "assets/theme";
+
+// Authentication
+import axios from "axios";
+import AuthContext, { AuthProvider } from "context/AuthContext";
+import jwtDecode from "jwt-decode";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+// Axios
+import MockAdapter from "axios-mock-adapter";
+
+// Component to test
+import Minesweeper from "projects/games/minesweeper";
+
+// Google Client ID
+const clientId = "416010689831-4lgodfsd3n7h84buas2s2mivevp2kdln.apps.googleusercontent.com";
+
+// Define Mocks
+// Setup axios mock
+const mock = new MockAdapter(axios);
+
+const commentData = {
+  comments: [
+    {
+      id: 1,
+      project: 4,
+      name: "Joe Biden",
+      email: "oldbiden@ineedhelp.com",
+      body: "I lost so bad!!!",
+      create: "2024-05-26T17:42:43.263337Z",
+      updated: "2024-05-26T17:42:43.263383Z",
+      active: true,
+      parent: null,
+    },
+  ],
+};
+
+jest.mock("services/baseService", () => ({
+  getRequest: jest.fn(),
+  postRequest: jest.fn(),
+}));
+jest.mock("services/googleService", () => ({
+  getGoogleUser: jest.fn(),
+}));
+
+mock.onGet("/comments").reply(200, { data: { results: commentData } });
+mock.onPost("/comments").reply(201, commentData);
+
+// Define Mocks
+jest.mock("jwt-decode");
+jest.mock("react-p5", () => {
+  const { forwardRef } = jest.requireActual("react");
+  return {
+    __esModule: true,
+    default: forwardRef(() => <div>Sketch</div>),
+  };
+});
+jest.mock("react-monaco-editor", () => {
+  const { forwardRef } = jest.requireActual("react");
+  return {
+    __esModule: true,
+    default: forwardRef(() => <div>Mock Editor</div>),
+  };
+});
+jest.mock("draft-convert", () => {
+  return {
+    convertFromHTML: jest.fn(),
+    convertToHTML: jest.fn(),
+    convertToRaw: jest.fn(),
+  };
+});
+jest.mock("react-router-dom", () => ({
+  Link: jest.fn(({ to, children }) => <a href={to}>{children}</a>),
+  useNavigate: jest.fn,
+}));
+jest.mock("examples/Navbars/DefaultNavbar", () => {
+  const { forwardRef } = jest.requireActual("react");
+  return {
+    __esModule: true,
+    default: forwardRef(() => <div>Mock Navbar</div>),
+  };
+});
+jest.mock("routes", () => []);
+
+describe("Minesweeper", () => {
+  beforeEach(() => {
+    localStorage.clear();
+  });
+
+  afterEach(() => {
+    localStorage.removeItem("authTokens");
+    jest.clearAllMocks();
+  });
+
+  it("adds a comment", () => {
+    localStorage.setItem(
+      "authTokens",
+      JSON.stringify({ access: mockToken, refresh: refreshToken })
+    );
+
+    // Set a mock payload for the decoded token
+    const mockPayload = { user: "Kamala Harris", exp: 1893456000 };
+    jwtDecode.mockReturnValue(mockPayload);
+
+    // Mock tokens
+    const mockToken = "mocked_jwt_value";
+    const refreshToken = "mocked_refresh_value";
+
+    const contextData = {
+      loginUser: jest.fn(),
+    };
+    const { getByRole, queryAllByText } = render(
+      <GoogleOAuthProvider clientId={clientId}>
+        <AuthContext.Provider value={contextData}>
+          <AuthProvider>
+            <ThemeProvider theme={theme}>
+              <Minesweeper />
+            </ThemeProvider>
+          </AuthProvider>
+        </AuthContext.Provider>
+      </GoogleOAuthProvider>
+    );
+
+    // Toggle plain text
+    const checkbox = getByRole("checkbox");
+    fireEvent.click(checkbox);
+
+    // Write something in there
+    const commentBox = getByRole("textbox");
+    fireEvent.change(commentBox, { target: { value: "Oh no!" } });
+
+    // Add the comment
+    const addButton = getByRole("button");
+    fireEvent.click(addButton);
+
+    expect(queryAllByText("Minesweeper").length).toBe(2);
+  });
+
+  it("renders without user", () => {
+    const contextData = {
+      loginUser: jest.fn(),
+    };
+
+    const { queryAllByText } = render(
+      <GoogleOAuthProvider clientId={clientId}>
+        <AuthContext.Provider value={contextData}>
+          <AuthProvider>
+            <ThemeProvider theme={theme}>
+              <Minesweeper />
+            </ThemeProvider>
+          </AuthProvider>
+        </AuthContext.Provider>
+      </GoogleOAuthProvider>
+    );
+    expect(queryAllByText("Minesweeper").length).toBe(2);
+  });
+});
