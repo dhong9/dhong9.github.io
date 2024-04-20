c49723c1b6d399b67c6c5b53b6704018ee83dc69
dhong9 <danielhong35@gmail.com>
2024-04-19 21:40:33 -0700

    Added UploadImage component

---
+++ src/pages/LandingPages/UploadImage/index.js
@@ -0,0 +1,23 @@
+/**
+=========================================================
+* Danyo-1.2
+=========================================================
+
+* Copyright 2024 Danyo (https://www.danyo.tech)
+
+Coded by www.danyo.tech
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+function UploadImage() {
+  return (
+    <div>
+      <h1>Upload Image</h1>
+    </div>
+  );
+}
+
+export default UploadImage;
