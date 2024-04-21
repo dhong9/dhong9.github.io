306c048ee8ac8beb4fc60375044ae8e44a84bf97
dhong9 <danielhong35@gmail.com>
2024-04-21 11:17:38 -0700

    UploadImage smoke test

---
+++ src/pages/LandingPages/UploadImage/UploadImage.test.js
@@ -0,0 +1,36 @@
+/**
+=========================================================
+* Danyo-1.2
+=========================================================
+
+* Copyright 2024 Danyo (https://www.danyo.tech)
+
+Coded by www.danyo.tech
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// React testing libraries
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+// Material Kit 2 React themes
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "assets/theme";
+
+// Authentication
+import UploadImage from "pages/LandingPages/UploadImage";
+
+describe("UploadImage", () => {
+  it("renders", () => {
+    const { container } = render(
+      <ThemeProvider theme={theme}>
+        <UploadImage />
+      </ThemeProvider>
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+});
