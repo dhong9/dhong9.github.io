441cdb96529d36be3d618df3a7227070ccb395ed
dhong9 <danielhong35@gmail.com>
2024-05-12 21:00:06 -0700

    Added Forgot Password page to layouts

---
+++ src/layouts/pages/authentication/forgot-password/index.js
@@ -0,0 +1,22 @@
+/**
+=========================================================
+* Danyo-1.2
+=========================================================
+
+* Copyright 2024 Danyo (https://www.danyo.tech)
+
+Coded by www.danyo.tech
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// Danyo pages
+import ForgotPassword from "pages/LandingPages/ForgotPassword";
+
+export default function ForgotPasswordPage() {
+  // onsuccess will be called by parent component
+
+  return <ForgotPassword />;
+}
