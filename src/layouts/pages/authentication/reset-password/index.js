6f19233d2be41e128b5b2fbe96ddb3db33ae7d52
dhong9 <danielhong35@gmail.com>
2024-05-17 16:07:41 -0700

    Added ResetPassword page to layouts

---
+++ src/layouts/pages/authentication/reset-password/index.js
@@ -0,0 +1,22 @@
+/**
+=========================================================
+* Danyo-1.2
+=========================================================
+
+* Copyright 2024 Danyo (https://www.danyo.tech)
+
+Coded by www.danyo.tech
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// Danyo pages
+import ResetPassword from "pages/LandingPages/ResetPassword";
+
+export default function ResetPasswordPage() {
+  // onsuccess will be called by parent component
+
+  return <ResetPassword />;
+}
