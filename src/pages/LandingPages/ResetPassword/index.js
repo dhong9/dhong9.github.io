1dd0fd8cbcab5fc89fe47767dc3e4c5544356587
dhong9 <danielhong35@gmail.com>
2024-05-17 16:05:34 -0700

    Added placeholder ResetPassword page

---
+++ src/pages/LandingPages/ResetPassword/index.js
@@ -0,0 +1,23 @@
+/**
+=========================================================
+* Danyo-1.2
+=========================================================
+
+* Copyright 2024 Danyo (https://www.danyo.tech)
+
+Coded by www.danyo.tech
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+function ResetPassword() {
+  return (
+    <div>
+      <h1>Reset Password</h1>
+    </div>
+  );
+}
+
+export default ResetPassword;
