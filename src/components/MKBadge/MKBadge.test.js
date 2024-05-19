60b61eab00b1ee8191f64f2c73b14c5b55493ca1
dhong9 <danielhong35@gmail.com>
2024-05-19 11:19:49 -0700

    Added unit test for small light badge

---
+++ src/components/MKBadge/MKBadge.test.js
@@ -0,0 +1,35 @@
+/**
+=========================================================
+* Danyo-1.2
+=========================================================
+
+* Copyright 2024 Danyo (https://www.danyo.tech)
+
+Coded by www.danyo.tech
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// React testing libraries
+import { render } from "@testing-library/react";
+
+// Material Kit 2 React themes
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "assets/theme";
+
+// Component to test
+import MKBadge from "components/MKBadge";
+
+describe("MKBadge", () => {
+  it("renders small light badges", () => {
+    const { container } = render(
+      <ThemeProvider theme={theme}>
+        <MKBadge color="light" size="small"></MKBadge>
+      </ThemeProvider>
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+});
