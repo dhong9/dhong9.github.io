9436a75a1a90b22bdf557a25f6cab4d53968c7fc
dhong9 <danielhong35@gmail.com>
2024-05-27 07:41:10 -0700

    Added unit test for ActivateAccount

--- src/context/AuthContext.test.js
+++ src/context/AuthContext.test.js
@@ -1,3 +1,17 @@
+/**
+=========================================================
+* Danyo-1.2
+=========================================================
+
+* Copyright 2024 Danyo (https://www.danyo.tech)
+
+Coded by www.danyo.tech
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
 // React testing libraries
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
---
+++ src/pages/LandingPages/ActivateAccount/ActivateAccount.test.js
@@ -0,0 +1,38 @@
+/**
+=========================================================
+* Danyo-1.2
+=========================================================
+
+* Copyright 2024 Danyo (https://www.danyo.tech)
+
+Coded by www.danyo.tech
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// Unit test libraries
+import { render } from "@testing-library/react";
+
+// Component to test
+import ActivateAccountBasic from "pages/LandingPages/ActivateAccount";
+
+// Axios
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
+
+// Setup axios mock
+const mock = new MockAdapter(axios);
+mock.onPost("accounts/users/activation/").reply(200, {});
+
+describe("ActivateAccount", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("renders correct text", () => {
+    const { queryByText } = render(<ActivateAccountBasic uid="R4x" token="bc312" />);
+    expect(queryByText("Activating account...")).toBeInTheDocument;
+  });
+});
