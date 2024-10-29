f9ea8d5bbc8475e606ef5a479f7c7815722e3a29
dhong9 <danielhong35@gmail.com>
2024-10-28 18:02:56 -0700

    Added Sketch object to VisualizeSubdivisions component

--- src/projects/artistTools/visualizeSubdivisions/index.js
+++ src/projects/artistTools/visualizeSubdivisions/index.js
@@ -19,16 +19,35 @@ import View from "layouts/sections/components/View";
 // Visualize subdivisions code
 import visualizeSubdivisionsCode from "projects/artistTools/visualizeSubdivisions/code";
 
+// p5
+import Sketch from "react-p5";
+
 function VisualizeSubdivisions() {
+  const setup = (p5, canvasParentRef) => {
+    // use parent to render the canvas in this ref
+    // (without that p5 will render the canvas outside of your component)
+    // Canvas dimension is based on div's size
+    const visualizeSubdivisions = document.querySelector(".codeOutput");
+    p5.createCanvas(visualizeSubdivisions.clientWidth, visualizeSubdivisions.clientHeight).parent(
+      canvasParentRef
+    );
+  };
+
+  const draw = (p5) => {
+    p5.background(200);
+  };
+
   return (
     <BaseLayout
-      title="Games"
+      title="Artist Tools"
       breadcrumb={[
         { label: "Artist Tools", route: "/sections/artistTools/2048" },
         { label: "Visualize Subdivisions" },
       ]}
     >
-      <View title="Visualize Subdivisions" code={visualizeSubdivisionsCode} height="40rem"></View>
+      <View title="Visualize Subdivisions" code={visualizeSubdivisionsCode} height="40rem">
+        <Sketch setup={setup} draw={draw} />
+      </View>
     </BaseLayout>
   );
 }
