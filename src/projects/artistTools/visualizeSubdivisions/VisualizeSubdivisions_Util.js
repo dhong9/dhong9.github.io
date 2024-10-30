10f497672be526380a9fec9c8b374ad95c1995df
dhong9 <danielhong35@gmail.com>
2024-10-29 20:17:34 -0700

    Removed self references

--- src/projects/artistTools/visualizeSubdivisions/VisualizeSubdivisions_Util.js
+++ src/projects/artistTools/visualizeSubdivisions/VisualizeSubdivisions_Util.js
@@ -14,20 +14,15 @@ Coded by www.danyo.tech
 
 export const getPolyPoints = (points) => {
   const polyPoints = [];
+
   for (let i = 0; i < points.length; i++) {
     const [x1, y1] = points[i];
-    const [x2, y2] = this.points[i + 1];
+    const [x2, y2] = points[i + 1];
     const midX = (x2 + x1) / 2,
       midY = (y2 + y1) / 2;
+    polyPoints.push(points[i]);
     polyPoints.push([midX, midY]);
   }
-
-  // Last point and first point
-  const [x1, y1] = this.points.at(-1);
-  const [x2, y2] = this.points[0];
-  const midX = (x2 + x1) / 2,
-    midY = (y2 + y1) / 2;
-  polyPoints.push([midX, midY]);
 
   return polyPoints;
 };
