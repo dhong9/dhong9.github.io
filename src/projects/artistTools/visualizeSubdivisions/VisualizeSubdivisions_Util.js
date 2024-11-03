c2ac4e3b4de1fb9bf18188dda55f115ecc3a3db8
dhong9 <danielhong35@gmail.com>
2024-11-02 17:12:48 -0700

    Weave lines on last 3 points

--- src/projects/artistTools/visualizeSubdivisions/VisualizeSubdivisions_Util.js
+++ src/projects/artistTools/visualizeSubdivisions/VisualizeSubdivisions_Util.js
@@ -87,5 +87,19 @@ export const buildStringArtObject = (points, subdivisions) => {
     subdivPoints2 = getSubdivPointsLine(polyPoint2, polyPoint3, subdivisions);
   subdivPointsLine.push(subdivPoints1, subdivPoints2);
 
+  netLines.push([polyPoint1[0], polyPoint1[1], subdivPoints2[0][0], subdivPoints2[0][1]]);
+
+  // Weave intermediate lines
+  for (let j = 0; j < subdivisions - 1; j++) {
+    netLines.push([
+      subdivPoints1[j][0],
+      subdivPoints1[j][1],
+      subdivPoints2[-~j][0],
+      subdivPoints2[-~j][1],
+    ]);
+  }
+
+  netLines.push([polyPoint3[0], polyPoint3[1], subdivPoints1.at(-1)[0], subdivPoints1.at(-1)[1]]);
+
   return { polyPoints, subdivPointsLine, netLines };
 };
