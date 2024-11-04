52f6faaf3c7efc8b2c20a73ab762bd427af44357
dhong9 <danielhong35@gmail.com>
2024-11-03 16:47:16 -0800

    Added main code to Visualize Subdivisons code tab

--- src/projects/artistTools/visualizeSubdivisions/code.js
+++ src/projects/artistTools/visualizeSubdivisions/code.js
@@ -98,6 +98,103 @@ const findIntersection = ([x1, y1, x2, y2], [x3, y3, x4, y4]) => {
   const y = m1 * x + b1;
   return [x, y];
 };
+
+/**
+ * Provides data needed to build string art for visualizing polygon subdivisions
+ * @param {Array<Number>} points polygon points
+ * @param {Number} subdivisions number of subdivisions
+ * @returns polygon points with midpoints, subdivision points, lines for weaving string art, resulting polygon points
+ */
+export const buildStringArtObject = (points, subdivisions) => {
+  const polyPoints = getPolyPoints(points);
+
+  // Move first point to the end
+  polyPoints.push(polyPoints.shift());
+
+  const subdivPointsLine = [];
+  const netLines = [];
+  const intersectionPoints = [];
+  for (let i = 0; i < polyPoints.length - 2; i += 2) {
+    const polyPoint1 = polyPoints[i],
+      polyPoint2 = polyPoints[i + 1],
+      polyPoint3 = polyPoints[i + 2];
+
+    const subdivPoints1 = getSubdivPointsLine(polyPoint1, polyPoint2, subdivisions),
+      subdivPoints2 = getSubdivPointsLine(polyPoint2, polyPoint3, subdivisions);
+    subdivPointsLine.push(subdivPoints1, subdivPoints2);
+
+    const tempNetLines = [];
+
+    tempNetLines.push([polyPoint1[0], polyPoint1[1], subdivPoints2[0][0], subdivPoints2[0][1]]);
+
+    // Weave intermediate lines
+    for (let j = 0; j < subdivisions - 1; j++) {
+      tempNetLines.push([
+        subdivPoints1[j][0],
+        subdivPoints1[j][1],
+        subdivPoints2[-~j][0],
+        subdivPoints2[-~j][1],
+      ]);
+    }
+
+    tempNetLines.push([
+      polyPoint3[0],
+      polyPoint3[1],
+      subdivPoints1.at(-1)[0],
+      subdivPoints1.at(-1)[1],
+    ]);
+
+    // Find intersection of each pair of net lines
+    intersectionPoints.push(polyPoint1);
+    for (let j = 0; j < tempNetLines.length - 1; j++) {
+      intersectionPoints.push(findIntersection(tempNetLines[j], tempNetLines[j + 1]));
+    }
+
+    netLines.push(...tempNetLines);
+  }
+
+  // Last 3 points
+  const polyPoint1 = polyPoints.at(-2),
+    polyPoint2 = polyPoints.at(-1),
+    polyPoint3 = polyPoints[0];
+  const subdivPoints1 = getSubdivPointsLine(polyPoint1, polyPoint2, subdivisions),
+    subdivPoints2 = getSubdivPointsLine(polyPoint2, polyPoint3, subdivisions);
+  subdivPointsLine.push(subdivPoints1, subdivPoints2);
+
+  const tempNetLines = [];
+  tempNetLines.push([polyPoint1[0], polyPoint1[1], subdivPoints2[0][0], subdivPoints2[0][1]]);
+
+  // Weave intermediate lines
+  for (let j = 0; j < subdivisions - 1; j++) {
+    tempNetLines.push([
+      subdivPoints1[j][0],
+      subdivPoints1[j][1],
+      subdivPoints2[-~j][0],
+      subdivPoints2[-~j][1],
+    ]);
+  }
+
+  tempNetLines.push([
+    polyPoint3[0],
+    polyPoint3[1],
+    subdivPoints1.at(-1)[0],
+    subdivPoints1.at(-1)[1],
+  ]);
+
+  // Find intersection of each pair of net lines
+  intersectionPoints.push(polyPoint1);
+  for (let j = 0; j < tempNetLines.length - 1; j++) {
+    intersectionPoints.push(findIntersection(tempNetLines[j], tempNetLines[j + 1]));
+  }
+
+  netLines.push(...tempNetLines);
+
+  // Close the polygon
+  intersectionPoints.push(intersectionPoints[0]);
+
+  return { polyPoints, subdivPointsLine, netLines, intersectionPoints };
+};
+
 `;
 
 export default visualizeSubdivisionsCode;
