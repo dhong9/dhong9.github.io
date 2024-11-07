bf65d763aa2ad89ee29d91199e376dc79f1b8e57
dhong9 <danielhong35@gmail.com>
2024-11-06 19:02:04 -0800

    Added placeholder page for color code converter

---
+++ src/projects/artistTools/colorCodeConverter/index.js
@@ -0,0 +1,23 @@
+/**
+=========================================================
+* Danyo-1.2
+=========================================================
+
+* Copyright 2024 Danyo (https://www.danyo.tech)
+
+Coded by www.danyo.tech
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+function ColorCodeConverter() {
+  return (
+    <div>
+      <h1>Color Code Converter</h1>
+    </div>
+  );
+}
+
+export default ColorCodeConverter;
