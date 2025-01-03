1c785d95fdca23ef9b47c0f974b8188f0c61443b
dhong9 <danielhong35@gmail.com>
2025-01-02 21:34:43 -0800

    Added spreadsheet cells background image

--- src/projects/artistTools/imageToSpreadsheet/index.js
+++ src/projects/artistTools/imageToSpreadsheet/index.js
@@ -1,8 +1,42 @@
+// Material Kit 2 React components
+import MKBox from "components/MKBox";
+
+// Material Kit 2 React example components
+import DefaultNavbar from "examples/Navbars/DefaultNavbar";
+import SimpleFooter from "examples/Footers/SimpleFooter";
+
+// Material Kit 2 React page layout routes
+import routes from "routes";
+
+// Images
+import bgImage from "assets/images/spreadsheetCells.png";
+
 function ImageToSpreadsheet() {
   return (
-    <div>
-      <h1>Image to Spreadsheet</h1>
-    </div>
+    <>
+      <DefaultNavbar routes={routes} transparent light />
+      <MKBox
+        position="absolute"
+        top={0}
+        left={0}
+        zIndex={1}
+        width="100%"
+        minHeight="100vh"
+        sx={{
+          backgroundImage: ({ functions: { linearGradient, rgba }, palette: { gradients } }) =>
+            `${linearGradient(
+              rgba(gradients.dark.main, 0.6),
+              rgba(gradients.dark.state, 0.6)
+            )}, url(${bgImage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      />
+      <MKBox width="100%" position="absolute" zIndex={2} bottom="1.625rem">
+        <SimpleFooter light />
+      </MKBox>
+    </>
   );
 }
 
